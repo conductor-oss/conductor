187f57566b8326640a7f8768c59250eea29f9c27
Viren Baraiya <vbaraiya@netflix.com>
2017-01-10 15:47:07 -0800

    sort the queues by size

--- jersey/src/main/java/com/netflix/conductor/server/resources/TaskResource.java
+++ jersey/src/main/java/com/netflix/conductor/server/resources/TaskResource.java
@@ -15,8 +15,13 @@
  */
 package com.netflix.conductor.server.resources;
 
+import java.util.Comparator;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+import java.util.TreeSet;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
@@ -155,7 +160,13 @@ public class TaskResource {
 	@ApiOperation("Get the details about each queue")
 	@Consumes({ MediaType.WILDCARD })
 	public Map<String, Long> all() throws Exception {
-		return queues.queuesDetail();
+		Map<String, Long> all = queues.queuesDetail();
+		Set<Entry<String, Long>> entries = all.entrySet();
+		Set<Entry<String, Long>> sorted = new TreeSet<>(Comparator.comparingLong(e -> e.getValue()));
+		sorted.addAll(entries);
+		LinkedHashMap<String, Long> sortedMap = new LinkedHashMap<>();
+		sorted.stream().map(e -> sortedMap.put(e.getKey(), e.getValue()));
+		return sortedMap;
 	}
 
 	@POST
