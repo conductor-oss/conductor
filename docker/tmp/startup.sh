36956e2d2e5741ae8854f6e1a06cce485b1b9124
Viren Baraiya <vbaraiya@netflix.com>
2017-01-18 18:03:22 -0800

    docker compose and docker file for conductor

--- docker/tmp/startup.sh
+++ docker/tmp/startup.sh
@@ -2,7 +2,7 @@
 
 
 # Wait for the DB to be up
-until ping -c1 dynomite 2>&1 >/dev/null; do
+until ping -c1 dyno1 2>&1 >/dev/null; do
   printf '.'
   sleep 0.1
 done
@@ -10,9 +10,9 @@ done
 echo "Successfully pinged dynomite."
 
 #Wait up to a minute for port 22122 to be open for business.
-if nc -z -w 60 dynomite 22122; then
+if nc -z -w 60 dyno1 22122; then
   echo "Could not connect to the database: $?."
-  exit 1
+  # exit 1
 else
   echo "Database port 22122 open."
 fi
@@ -26,5 +26,3 @@ npm run watch &
 # Start the server
 cd /
 java -jar conductor-server-*-all.jar config.properties
-
-
