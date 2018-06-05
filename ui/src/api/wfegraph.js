40a8a4a7e040ecc8c7e68278a82791bbab058541
Anoop Panicker <apanicker@netflix.com>
2018-06-05 10:38:40 -0700

    Revert "Revert "Added FAILED_WITH_TERMINAL_ERROR status to show up in the workflow graph""
    This reverts commit ee2f3dedb8e9345c5ebbfd334740ba7d4c7a360b.

--- ui/src/api/wfegraph.js
+++ ui/src/api/wfegraph.js
@@ -61,6 +61,7 @@ class Workflow2Graph {
         case 'TIMED_OUT':
         case 'CANCELLED':
         case 'CANCELED':
+        case 'FAILED_WITH_TERMINAL_ERROR':
           style = 'stroke: #ff0000; fill: #ff0000';
           labelStyle = 'fill:#ffffff; stroke-width: 1px';
           break;
