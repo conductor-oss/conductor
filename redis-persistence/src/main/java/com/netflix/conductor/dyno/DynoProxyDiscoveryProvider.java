8223b398991726336e410b0fcc0db5c5d360ea25
Greg Orzell <gorzell@github.com>
2018-06-07 11:32:45 +0200

    Minor review feedback.

--- redis-persistence/src/main/java/com/netflix/conductor/dyno/DynoProxyDiscoveryProvider.java
+++ redis-persistence/src/main/java/com/netflix/conductor/dyno/DynoProxyDiscoveryProvider.java
@@ -1,6 +1,5 @@
 package com.netflix.conductor.dyno;
 
-import com.netflix.conductor.core.config.Configuration;
 import com.netflix.discovery.DiscoveryClient;
 import com.netflix.dyno.jedis.DynoJedisClient;
 
@@ -11,17 +10,17 @@ import redis.clients.jedis.JedisCommands;
 
 public class DynoProxyDiscoveryProvider implements Provider<JedisCommands> {
     private final DiscoveryClient discoveryClient;
-    private final Configuration configuration;
+    private final DynomiteConfiguration configuration;
 
     @Inject
-    public DynoProxyDiscoveryProvider(DiscoveryClient discoveryClient, Configuration configuration) {
+    public DynoProxyDiscoveryProvider(DiscoveryClient discoveryClient, DynomiteConfiguration configuration) {
         this.discoveryClient = discoveryClient;
         this.configuration = configuration;
     }
 
     @Override
     public JedisCommands get() {
-        String cluster = configuration.getProperty("workflow.dynomite.cluster", null);
+        String cluster = configuration.getCluster();
         String applicationName = configuration.getAppId();
         return new DynoJedisClient
                 .Builder()
