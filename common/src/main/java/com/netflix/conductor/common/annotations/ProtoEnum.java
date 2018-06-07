1beeecc6b7b1a61057dc2368867584f4cc7cfd54
Vicent Marti <vmg@strn.cat>
2018-06-07 12:40:21 +0200

    common: Document the ProtoBuf-related annotations

--- common/src/main/java/com/netflix/conductor/common/annotations/ProtoEnum.java
+++ common/src/main/java/com/netflix/conductor/common/annotations/ProtoEnum.java
@@ -5,6 +5,10 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * ProtoEnum annotates an enum type that will be exposed via the GRPC
+ * API as a native Protocol Buffers enum.
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 public @interface ProtoEnum {
