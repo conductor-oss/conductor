a71e26c22b03d3296277225ec44ff0c7bf6ed751
Greg Orzell <gorzell@github.com>
2018-06-11 11:02:39 +0200

    Cherry pick bootstrapping merge.

--- grpc-server/src/main/java/com/netflix/conductor/grpc/server/GRPCModule.java
+++ grpc-server/src/main/java/com/netflix/conductor/grpc/server/GRPCModule.java
@@ -2,7 +2,12 @@ package com.netflix.conductor.grpc.server;
 
 import com.google.inject.AbstractModule;
 import com.google.inject.Provides;
+
 import com.netflix.conductor.core.config.Configuration;
+import com.netflix.conductor.core.config.CoreModule;
+import com.netflix.conductor.core.config.SystemPropertiesConfiguration;
+import com.netflix.conductor.dao.es5.index.ElasticSearchModuleV5;
+import com.netflix.conductor.dao.mysql.MySQLWorkflowModule;
 import com.netflix.conductor.grpc.EventServiceGrpc;
 import com.netflix.conductor.grpc.MetadataServiceGrpc;
 import com.netflix.conductor.grpc.TaskServiceGrpc;
@@ -11,28 +16,28 @@ import com.netflix.conductor.grpc.WorkflowServiceGrpc;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.atomic.AtomicInteger;
 
+import javax.inject.Singleton;
+
 public class GRPCModule extends AbstractModule {
 
     // FIXME Eventually this should be shared with the Jersey code and provided by the server module.
     private final int maxThreads = 50;
-    private final Configuration configuration;
     private ExecutorService es;
 
-    public GRPCModule(Configuration configuration){
-        this.configuration = configuration;
-    }
 
     @Override
     protected void configure() {
         configureExecutorService();
 
-        bind(Configuration.class).toInstance(configuration);
+        install(new CoreModule());
+        install(new ElasticSearchModuleV5());
+        install(new MySQLWorkflowModule());
+        bind(Configuration.class).to(SystemPropertiesConfiguration.class).in(Singleton.class);
         bind(TaskServiceGrpc.TaskServiceImplBase.class).to(TaskServiceImpl.class);
         bind(MetadataServiceGrpc.MetadataServiceImplBase.class).to(MetadataServiceImpl.class);
         bind(WorkflowServiceGrpc.WorkflowServiceImplBase.class).to(WorkflowServiceImpl.class);
+        bind(GRPCServer.class).toProvider(GRPCServerProvider.class).asEagerSingleton();
         bind(EventServiceGrpc.EventServiceImplBase.class).to(EventServiceImpl.class);
-
-        bind(GRPCServer.class).to(GRPCServer.class);
     }
 
     @Provides
