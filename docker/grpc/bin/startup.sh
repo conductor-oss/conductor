d568659d7f17cec42bf7c12b2377091a82d268ac
Greg Orzell <gorzell@github.com>
2018-06-12 15:12:35 +0200

    Remove ignore file entry that was ignoring valid config files.

---
+++ docker/grpc/bin/startup.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+# startup.sh - startup script for the server docker image
+
+echo "Starting Conductor server"
+
+# Start the server
+cd /app/libs
+echo "Property file: $CONFIG_PROP"
+echo $CONFIG_PROP
+export config_file=
+
+if [ -z "$CONFIG_PROP" ];
+  then
+    echo "Using an in-memory instance of conductor";
+    export config_file=/app/config/config-local.properties
+  else
+    echo "Using '$CONFIG_PROP'";
+    export config_file=/app/config/$CONFIG_PROP
+fi
+
+java -jar conductor-grpc-server-*-all.jar $config_file
