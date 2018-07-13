ac38fae9289bc3ada34ed7c10679f71a5053903c
Sean Willison <sean.willison@bcbsfl.com>
2018-07-13 14:26:55 -0400

    created test for auth header not present.

--- ui/test/filters.test.js
+++ ui/test/filters.test.js
@@ -5,7 +5,7 @@ describe('Filters', function() {
   describe('Pre Middleware', function() {
     const authFilter = new AuthFilter();
 
-    it('should add authHeader to req and call next', function() {
+    it('should add token to req and call next', function() {
       const middleware = [];
 
       // Create Mock App
@@ -14,7 +14,35 @@ describe('Filters', function() {
           middleware.push(f);
         }
       };
-      
+
+      // Add the middleware
+      authFilter.init(app);
+
+      // Create the mock request
+      const req = {
+        headers: {
+          authorization: 'Bearer header.body.signature'
+        }
+      };
+
+      const res = {};
+
+      // Execute the auth middleware
+      middleware[0](req, res, () => {
+        assert.equal(req.headers.authorization, req.token);
+      });
+    });
+
+    it('should bypass add auth token if auth header not present and call next', function() {
+      const middleware = [];
+
+      // Create Mock App
+      const app = {
+        use: f => {
+          middleware.push(f);
+        }
+      };
+
       // Add the middleware
       authFilter.init(app);
 
