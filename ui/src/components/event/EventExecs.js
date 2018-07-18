6de33b0fb5685c57b84bff97448f5bf08ae205cd
Sean Willison <sean.willison@bcbsfl.com>
2018-07-18 14:01:21 -0400

    createClass() is not supported in newer versions of react, refactoring in preparation for upgrade - EventExecs.js

--- ui/src/components/event/EventExecs.js
+++ ui/src/components/event/EventExecs.js
@@ -1,29 +1,28 @@
 import React from 'react';
-import { Table, Grid, Row, Col } from 'react-bootstrap';
 import { connect } from 'react-redux';
-import { getEventHandlers } from '../../actions/WorkflowActions';
 import Typeahead from 'react-bootstrap-typeahead';
+import { Table, Grid, Row, Col } from 'react-bootstrap';
+import { getEventHandlers } from '../../actions/WorkflowActions';
 
-const EventExecs = React.createClass({
-  getInitialState() {
-    return {
-      events: [],
-      executions: [],
-      eventTypes: []
-    }
-  },
-  componentWillMount(){
+class EventExecs extends React.Component {
+  state = {
+    events: [],
+    executions: [],
+    eventTypes: []
+  };
+
+  componentWillMount() {
     this.props.dispatch(getEventHandlers());
-    //this.props.dispatch(getEvents());
-  },
+  }
+
   componentWillReceiveProps(nextProps) {
     this.state.executions = nextProps.executions;
     this.state.events = nextProps.events;
-  },
+  }
+
   render() {
-    let executions = this.state.executions;
-    let events = this.state.events;
-    let eventTypes = [];
+    const { events } = this.state;
+    const eventTypes = [];
     events.forEach(event => {
       eventTypes.push(event.event);
     });
@@ -32,27 +31,33 @@ const EventExecs = React.createClass({
       <div className="ui-content">
         <h1>Event Executions</h1>
         <h4>Search for Event Executions</h4>
-        <Grid fluid={true}>
+        <Grid fluid>
           <Row className="show-grid">
             <Col md={8}>
-              <Typeahead ref="eventTypes"  options={eventTypes} placeholder="Filter by Event" multiple={true} selected={this.state.eventTypes}/>
+              <Typeahead
+                ref="eventTypes"
+                options={eventTypes}
+                placeholder="Filter by Event"
+                multiple
+                selected={this.state.eventTypes}
+              />
             </Col>
             <Col md={4}>
-              <Input type="text" ref="q" placeholder="search..."></Input>
+              <Input type="text" ref="q" placeholder="search..." />
             </Col>
           </Row>
         </Grid>
-        <br/>
-        <Table responsive={true} striped={true} hover={true} condensed={false} bordered={true}>
-        <thead>
-          <tr>
-            <th>Something here</th>
-          </tr>
-        </thead>
+        <br />
+        <Table responsive striped hover condensed bordered>
+          <thead>
+            <tr>
+              <th>Something here</th>
+            </tr>
+          </thead>
         </Table>
       </div>
     );
   }
-});
+}
 
 export default connect(state => state.workflow)(EventExecs);
