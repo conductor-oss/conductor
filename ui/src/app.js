00f53d693ff852099f4d05dc06fe8273930154d7
Sean Willison <sean.willison@bcbsfl.com>
2018-07-18 13:46:32 -0400

    linting corrections - app.js

--- ui/src/app.js
+++ ui/src/app.js
@@ -1,28 +1,27 @@
 import React from 'react';
-import {render} from 'react-dom';
-import { Router, browserHistory } from 'react-router'
+import { render } from 'react-dom';
+import { Router, browserHistory } from 'react-router';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunkMiddleware from 'redux-thunk';
 import routeConfig from './routes';
-import { Provider } from 'react-redux'
-import { createStore, applyMiddleware, compose } from 'redux'
-import thunkMiddleware from 'redux-thunk'
-import workflowApp from './reducers'
-import { syncHistoryWithStore, routerReducer } from 'react-router-redux'
+import workflowApp from './reducers';
 
+// eslint-disable-next-line no-underscore-dangle
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-let store = createStore(workflowApp, composeEnhancers(applyMiddleware(
-  thunkMiddleware
-)));
+const store = createStore(workflowApp, composeEnhancers(applyMiddleware(thunkMiddleware)));
 
 function updateLocation() {
   store.dispatch({
     type: 'LOCATION_UPDATED',
-    'location' : this.state.location.key
+    location: this.state.location.key
   });
 }
 
 render(
   <Provider store={store}>
-    <Router history={browserHistory} routes={routeConfig} onUpdate={updateLocation}/>
+    <Router history={browserHistory} routes={routeConfig} onUpdate={updateLocation} />
   </Provider>,
-  document.getElementById('content'))
+  document.getElementById('content')
+);
