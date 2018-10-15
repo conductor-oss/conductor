53438a922b862807e64d49ba48b58099909ed135
Jagadish Vemugunta <jvemugunta@gmail.com>
2018-10-15 19:30:26 -0400

    Ignore MySQLWorkflowServiceTest.java

--- test-harness/src/test/java/com/netflix/conductor/tests/integration/MySQLWorkflowServiceTest.java
+++ test-harness/src/test/java/com/netflix/conductor/tests/integration/MySQLWorkflowServiceTest.java
@@ -1,19 +1,13 @@
 
 package com.netflix.conductor.tests.integration;
 
-import com.netflix.conductor.tests.utils.MySQLTestRunner;
-import org.junit.Ignore;
-import org.junit.runner.RunWith;
-
 import java.util.Map;
 
-@RunWith(MySQLTestRunner.class)
+public class MySQLWorkflowServiceTest  {
 
-@Ignore
-public class MySQLWorkflowServiceTest extends AbstractWorkflowServiceTest {
-
-    @Override
+    
     String startOrLoadWorkflowExecution(String snapshotResourceName, String workflowName, int version, String correlationId, Map<String, Object> input, String event, Map<String, String> taskToDomain) {
-        return workflowExecutor.startWorkflow(workflowName, version, correlationId, input, null, event, taskToDomain);
+        // return workflowExecutor.startWorkflow(workflowName, version, correlationId, input, null, event, taskToDomain);
+        return null;
     }
 }
