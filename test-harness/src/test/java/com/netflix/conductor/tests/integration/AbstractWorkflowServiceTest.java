651b50f8761656fbee74d3cc2e2852357535c85b
copierrj <copierrj@users.noreply.github.com>
2018-10-19 00:28:52 +0200

    Do not convert Number instances in InputData to String

--- test-harness/src/test/java/com/netflix/conductor/tests/integration/AbstractWorkflowServiceTest.java
+++ test-harness/src/test/java/com/netflix/conductor/tests/integration/AbstractWorkflowServiceTest.java
@@ -3488,6 +3488,9 @@ public abstract class AbstractWorkflowServiceTest {
         assertNotNull(task);
         assertNotNull(task.getOutputData());
         assertNotNull("Output: " + task.getOutputData().toString() + ", status: " + task.getStatus(), task.getOutputData().get("subWorkflowId"));
+        assertNotNull(task.getInputData());
+        assertTrue(task.getInputData().containsKey("workflowInput"));
+        assertEquals(42, ((Map<String, Object>)task.getInputData().get("workflowInput")).get("param2"));
         String subWorkflowId = task.getOutputData().get("subWorkflowId").toString();
 
         es = workflowExecutionService.getExecutionStatus(subWorkflowId, true);
@@ -4153,6 +4156,7 @@ public abstract class AbstractWorkflowServiceTest {
         Map<String, Object> ip2 = new HashMap<>();
         ip2.put("test", "test value");
         ip2.put("param1", "sub workflow input param1");
+        ip2.put("param2", 42);
         wft2.setInputParameters(ip2);
         wft2.setTaskReferenceName("a2");
 
