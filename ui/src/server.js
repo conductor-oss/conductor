3a0fb4988b4688e798587e5d54550d8a4866e7ed
Alex Lich <aliahushevich@netflix.com>
2018-12-04 17:15:35 -0800

    allow conductor to run on localhost only

--- ui/src/server.js
+++ ui/src/server.js
@@ -39,7 +39,7 @@ class Main {
   };
 
   startServer = app => {
-    const server = app.listen(process.env.NODE_PORT || 5000, () => {
+    const server = app.listen(process.env.NODE_PORT || 5000, 'localhost', function () {
       const { address, port } = server.address();
       log.info('Workflow UI listening at http://%s:%s', address, port);
       if (process.send) {
