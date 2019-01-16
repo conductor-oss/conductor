1069af36ae57729ce1c9869b7444bf9f203a5e93
tafael <tafaelalvescaixeta@gmail.com>
2019-01-16 15:03:51 -0200

    Added RestClientProvider

--- es6-persistence/src/main/java/com/netflix/conductor/elasticsearch/ElasticSearchModule.java
+++ es6-persistence/src/main/java/com/netflix/conductor/elasticsearch/ElasticSearchModule.java
@@ -4,6 +4,7 @@ import com.google.inject.AbstractModule;
 import com.google.inject.Singleton;
 import com.netflix.conductor.elasticsearch.es6.ElasticSearchV6Module;
 import org.elasticsearch.client.Client;
+import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestClientBuilder;
 
 public class ElasticSearchModule extends AbstractModule {
@@ -14,6 +15,8 @@ public class ElasticSearchModule extends AbstractModule {
 
         bind(ElasticSearchConfiguration.class).to(SystemPropertiesElasticSearchConfiguration.class);
         bind(Client.class).toProvider(ElasticSearchTransportClientProvider.class).in(Singleton.class);
+
+        bind(RestClient.class).toProvider(ElasticSearchRestClientProvider.class).in(Singleton.class);
         bind(RestClientBuilder.class).toProvider(ElasticSearchRestClientBuilderProvider.class).in(Singleton.class);
 
         install(new ElasticSearchV6Module(esConfiguration));
