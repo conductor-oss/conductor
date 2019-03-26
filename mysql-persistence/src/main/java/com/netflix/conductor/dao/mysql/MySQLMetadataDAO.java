13a407e7fe19cec007cc991e937ce77d6b725504
Ismaley Viana <ismaley.viana@gmail.com>
2019-03-26 14:54:24 -0300

    some cleanup

--- mysql-persistence/src/main/java/com/netflix/conductor/dao/mysql/MySQLMetadataDAO.java
+++ mysql-persistence/src/main/java/com/netflix/conductor/dao/mysql/MySQLMetadataDAO.java
@@ -312,7 +312,7 @@ public class MySQLMetadataDAO extends MySQLBaseDAO implements MetadataDAO {
     }
 
     /**
-     * Terminate the latest version that exists for the provided {@link WorkflowDef}.
+     * Return the latest version that exists for the provided {@link WorkflowDef}.
      *
      * @param tx  The {@link Connection} to use for queries.
      * @param def The {@code WorkflowDef} to check for.
