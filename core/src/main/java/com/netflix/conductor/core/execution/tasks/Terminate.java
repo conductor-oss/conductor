771a8f69ffca893f3ca27b1f6d7fcd07d9c7d439
Ismaley Viana <ismaley.viana@gmail.com>
2019-03-26 11:57:46 -0300

    adding some documentation
    (cherry picked from commit 978f207ec80ea4cbf5b9153902dda8df5c432ed9)

--- core/src/main/java/com/netflix/conductor/core/execution/tasks/Terminate.java
+++ core/src/main/java/com/netflix/conductor/core/execution/tasks/Terminate.java
@@ -12,6 +12,32 @@ import java.util.Map;
 import static com.netflix.conductor.common.run.Workflow.WorkflowStatus.COMPLETED;
 import static com.netflix.conductor.common.run.Workflow.WorkflowStatus.FAILED;
 
+/**
+ * Task that can terminate a workflow with a given status and modify the workflow's output with a given parameter,
+ * it can act as a "return" statement for conditions where you simply want to terminate your workflow. For example,
+ * if you have a decision where the first condition is met, you want to execute some tasks, otherwise you want to finish
+ * your workflow.
+ * <pre>
+...
+ {
+     "tasks": [{
+         "name": "terminate",
+         "taskReferenceName": "terminate0",
+         "inputParameters": {
+             "terminationStatus": "COMPLETED",
+             "workflowOutput": "${workflow.input}"
+         },
+         "type": "TERMINATE",
+         "startDelay": 0,
+         "optional": false
+     }]
+ }
+...
+ * </pre>
+ * This task has some validations on creation and execution, they are:
+ * - the "terminationStatus" parameter is mandatory and it can only receive the values "COMPLETED" or "FAILED"
+ * - the terminate task cannot be optional
+ */
 public class Terminate extends WorkflowSystemTask {
 
     private static final Logger logger = LoggerFactory.getLogger(Terminate.class);
