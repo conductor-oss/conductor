7362855693632198fba88d69a40e65e8cf4004d7
apau <apau@netflix.com>
2019-05-30 17:45:15 -0700

    fix Guice binding issue

--- core/src/main/java/com/netflix/conductor/core/config/NoopLockModule.java
+++ core/src/main/java/com/netflix/conductor/core/config/NoopLockModule.java
@@ -1,27 +1,18 @@
 package com.netflix.conductor.core.config;
 
 import com.google.inject.AbstractModule;
-import com.google.inject.name.Names;
+import com.google.inject.Provides;
 import com.netflix.conductor.core.utils.Lock;
 import com.netflix.conductor.core.utils.NoopLock;
 
-import javax.inject.Provider;
-
 public class NoopLockModule extends AbstractModule {
 
-    private static class LockProvider implements Provider<Lock> {
-
-        @Override
-        public Lock get() {
-            return new NoopLock();
-        }
-    }
-
     @Override
-    protected void configure() {
-        bind(Lock.class)
-                .annotatedWith(Names.named("executionLock"))
-                .toProvider(LockProvider.class);
+    protected void configure() {}
+
+    @Provides
+    protected Lock provideLock() {
+        return new NoopLock();
     }
 
 }
