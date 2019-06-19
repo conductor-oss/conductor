020466c72d9cf00aa911ac34c59343b23cad0890
apau <apau@netflix.com>
2019-06-18 17:59:05 -0700

    debugging, will clean up later.

--- zookeeper-lock/src/main/java/com/netflix/conductor/zookeeper/config/SystemPropertiesZookeeperConfiguration.java
+++ zookeeper-lock/src/main/java/com/netflix/conductor/zookeeper/config/SystemPropertiesZookeeperConfiguration.java
@@ -3,10 +3,4 @@ package com.netflix.conductor.zookeeper.config;
 import com.netflix.conductor.core.config.SystemPropertiesConfiguration;
 
 public class SystemPropertiesZookeeperConfiguration extends SystemPropertiesConfiguration implements ZookeeperConfiguration {
-
-    public String ZK_CONNECTION = "zk.connection";
-
-    public String getZkConnection() {
-        return getProperty(ZK_CONNECTION, "localhost:2181");
-    }
 }
--- zookeeper-lock/src/main/java/com/netflix/conductor/zookeeper/config/ZookeeperConfiguration.java
+++ zookeeper-lock/src/main/java/com/netflix/conductor/zookeeper/config/ZookeeperConfiguration.java
@@ -3,5 +3,9 @@ package com.netflix.conductor.zookeeper.config;
 import com.netflix.conductor.core.config.Configuration;
 
 public interface ZookeeperConfiguration extends Configuration {
-    String getZkConnection();
+    String ZK_CONNECTION = "zk.connection";
+
+    default String getZkConnection() {
+        return getProperty(ZK_CONNECTION, "localhost:2181");
+    }
 }
