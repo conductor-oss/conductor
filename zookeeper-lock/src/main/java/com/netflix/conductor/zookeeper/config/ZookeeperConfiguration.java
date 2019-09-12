de2494b4d09390db53c94665de5389c100dc6055
Kishore <kbanala@netflix.com>
2019-09-11 17:37:32 -0700

    Configurable zk timeouts (#1312)
    * Added configurable timeouts for Zookeeper.
    
    * Rename config parameters.

--- zookeeper-lock/src/main/java/com/netflix/conductor/zookeeper/config/ZookeeperConfiguration.java
+++ zookeeper-lock/src/main/java/com/netflix/conductor/zookeeper/config/ZookeeperConfiguration.java
@@ -1,11 +1,22 @@
 package com.netflix.conductor.zookeeper.config;
 
 import com.netflix.conductor.core.config.Configuration;
+import org.apache.curator.framework.CuratorFrameworkFactory;
 
 public interface ZookeeperConfiguration extends Configuration {
     String ZK_CONNECTION = "zk.connection";
+    String ZK_SESSIONTIMEOUT_MS = "zk.sessionTimeoutMs";
+    String ZK_CONNECTIONTIMEOUT_MS = "zk.connectionTimeoutMs";
 
     default String getZkConnection() {
         return getProperty(ZK_CONNECTION, "localhost:2181");
+    }
+
+    default int getZkSessiontimeoutMs() {
+        return getIntProperty(ZK_SESSIONTIMEOUT_MS, CuratorFrameworkFactory.builder().getSessionTimeoutMs());
+    }
+
+    default int getZkConnectiontimeoutMs() {
+        return getIntProperty(ZK_CONNECTIONTIMEOUT_MS, CuratorFrameworkFactory.builder().getConnectionTimeoutMs());
     }
 }
