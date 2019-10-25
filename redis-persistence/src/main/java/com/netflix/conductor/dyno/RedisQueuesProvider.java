3ed60fc043b23c8f0507db4f615652c59702547e
Anoop Panicker <apanicker@netflix.com>
2019-10-25 13:47:45 -0700

    upgrade dyno queues and jedis version

--- redis-persistence/src/main/java/com/netflix/conductor/dyno/RedisQueuesProvider.java
+++ redis-persistence/src/main/java/com/netflix/conductor/dyno/RedisQueuesProvider.java
@@ -1,16 +1,25 @@
+/*
+ * Copyright 2019 Netflix, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
 package com.netflix.conductor.dyno;
 
 import com.netflix.dyno.queues.ShardSupplier;
 import com.netflix.dyno.queues.redis.RedisQueues;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import javax.inject.Inject;
 import javax.inject.Named;
 import javax.inject.Provider;
-
-import redis.clients.jedis.JedisCommands;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import redis.clients.jedis.commands.JedisCommands;
 
 public class RedisQueuesProvider implements Provider<RedisQueues> {
 
