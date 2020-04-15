5cecf139e791c92502370444c5a6623254bf0eca
Anoop Panicker <apanicker@netflix.com>
2020-04-15 11:34:53 -0700

    add configuration to enable external index management in es6

--- es6-persistence/src/main/java/com/netflix/conductor/elasticsearch/EmbeddedElasticSearch.java
+++ es6-persistence/src/main/java/com/netflix/conductor/elasticsearch/EmbeddedElasticSearch.java
@@ -1,16 +1,26 @@
+/*
+ * Copyright 2020 Netflix, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
 package com.netflix.conductor.elasticsearch;
 
 import com.netflix.conductor.service.Lifecycle;
-
-import org.apache.commons.io.FileUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import org.apache.commons.io.FileUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public interface EmbeddedElasticSearch extends Lifecycle {
     Logger logger = LoggerFactory.getLogger(EmbeddedElasticSearch.class);
