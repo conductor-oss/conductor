167180ad517ef8d65679e2094ad3a63f4920cd5b
Anoop Panicker <apanicker@netflix.com>
2020-04-15 11:34:53 -0700

    add configuration to enable external index management in es6

--- es6-persistence/src/main/java/com/netflix/conductor/dao/es6/index/ElasticSearchBaseDAO.java
+++ es6-persistence/src/main/java/com/netflix/conductor/dao/es6/index/ElasticSearchBaseDAO.java
@@ -1,18 +1,29 @@
+/*
+ * Copyright 2020 Netflix, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
 package com.netflix.conductor.dao.es6.index;
 
 import com.netflix.conductor.dao.IndexDAO;
 import com.netflix.conductor.dao.es6.index.query.parser.Expression;
 import com.netflix.conductor.elasticsearch.query.parser.ParserException;
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.elasticsearch.index.query.BoolQueryBuilder;
 import org.elasticsearch.index.query.QueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.index.query.QueryStringQueryBuilder;
-
-import java.io.IOException;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 abstract class ElasticSearchBaseDAO implements IndexDAO {
 
@@ -45,8 +56,7 @@ abstract class ElasticSearchBaseDAO implements IndexDAO {
         return QueryBuilders.boolQuery().must(stringQuery).must(filterQuery);
     }
 
-    String indexName(String documentType) {
+    protected String getIndexName(String documentType) {
         return indexPrefix + "_" + documentType;
     }
-
 }
--- es6-persistence/src/main/java/com/netflix/conductor/elasticsearch/es6/ElasticSearchV6Module.java
+++ es6-persistence/src/main/java/com/netflix/conductor/elasticsearch/es6/ElasticSearchV6Module.java
@@ -1,5 +1,5 @@
-/**
- * Copyright 2016 Netflix, Inc.
+/*
+ * Copyright 2020 Netflix, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
@@ -10,22 +10,16 @@
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  */
-/**
- *
- */
 package com.netflix.conductor.elasticsearch.es6;
 
 import com.google.inject.AbstractModule;
-
 import com.netflix.conductor.dao.IndexDAO;
 import com.netflix.conductor.dao.es6.index.ElasticSearchDAOV6;
 import com.netflix.conductor.dao.es6.index.ElasticSearchRestDAOV6;
 import com.netflix.conductor.elasticsearch.ElasticSearchConfiguration;
 import com.netflix.conductor.elasticsearch.EmbeddedElasticSearchProvider;
-
 import java.util.HashSet;
 import java.util.Set;
-
 
 /**
  * @author Viren
