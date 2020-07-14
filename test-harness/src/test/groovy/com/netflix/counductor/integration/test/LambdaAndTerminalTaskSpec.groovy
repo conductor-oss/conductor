81b2fdd302ae68df43c86ee9db1240dabfbe7ee1
Poorna Reddy <preddy@netflix.com>
2020-07-14 14:48:31 -0700

    clean up

--- test-harness/src/test/groovy/com/netflix/counductor/integration/test/LambdaAndTerminalTaskSpec.groovy
+++ test-harness/src/test/groovy/com/netflix/counductor/integration/test/LambdaAndTerminalTaskSpec.groovy
@@ -1,11 +1,25 @@
+/*
+ * Copyright 2020 Netflix, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.netflix.counductor.integration.test
 
 import com.netflix.conductor.common.metadata.tasks.Task
 import com.netflix.conductor.common.run.Workflow
 import com.netflix.conductor.core.execution.WorkflowExecutor
-import com.netflix.conductor.core.execution.WorkflowSweeper
 import com.netflix.conductor.service.ExecutionService
-import com.netflix.conductor.service.MetadataService
 import com.netflix.conductor.test.util.WorkflowTestUtil
 import com.netflix.conductor.tests.utils.TestModule
 import com.netflix.governator.guice.test.ModulesForTesting
@@ -21,13 +35,7 @@ class LambdaAndTerminalTaskSpec extends Specification {
     ExecutionService workflowExecutionService
 
     @Inject
-    MetadataService metadataService
-
-    @Inject
     WorkflowExecutor workflowExecutor
-
-    @Inject
-    WorkflowSweeper workflowSweeper
 
     @Inject
     WorkflowTestUtil workflowTestUtil
@@ -38,6 +46,7 @@ class LambdaAndTerminalTaskSpec extends Specification {
     @Shared
     def WORKFLOW_WITH_TERMINATE_TASK_FAILED = 'test_terminate_task_failed_wf'
 
+    @Shared
     def  WORKFLOW_WITH_LAMBDA_TASK = 'test_lambda_wf'
 
     def setup() {
@@ -63,7 +72,6 @@ class LambdaAndTerminalTaskSpec extends Specification {
                 '', workflowInput, null, null, null)
 
         then: "Ensure that the workflow has started and the first task is in scheduled state"
-        workflowInstanceId
         with(workflowExecutionService.getExecutionStatus(workflowInstanceId, true)) {
             status == Workflow.WorkflowStatus.COMPLETED
             tasks.size() == 2
--- test-harness/src/test/groovy/com/netflix/counductor/integration/test/WaitWorkflowSpec.groovy
+++ test-harness/src/test/groovy/com/netflix/counductor/integration/test/WaitWorkflowSpec.groovy
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2020 Netflix, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.netflix.counductor.integration.test
 
 import com.netflix.conductor.common.metadata.tasks.Task
@@ -5,9 +20,7 @@ import com.netflix.conductor.common.metadata.tasks.TaskResult
 import com.netflix.conductor.common.metadata.workflow.TaskType
 import com.netflix.conductor.common.run.Workflow
 import com.netflix.conductor.core.execution.WorkflowExecutor
-import com.netflix.conductor.core.execution.WorkflowSweeper
 import com.netflix.conductor.service.ExecutionService
-import com.netflix.conductor.service.MetadataService
 import com.netflix.conductor.test.util.WorkflowTestUtil
 import com.netflix.conductor.tests.utils.TestModule
 import com.netflix.governator.guice.test.ModulesForTesting
@@ -25,13 +38,7 @@ class WaitWorkflowSpec extends Specification {
     ExecutionService workflowExecutionService
 
     @Inject
-    MetadataService metadataService
-
-    @Inject
     WorkflowExecutor workflowExecutor
-
-    @Inject
-    WorkflowSweeper workflowSweeper
 
     @Inject
     WorkflowTestUtil workflowTestUtil
