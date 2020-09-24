5a95b5f58bd1b05c70cef515851a060e88bdfab3
Andrea11 <andry93.mail@gmail.com>
2020-09-24 13:19:52 +0200

    update jq dependency to latest stable version

--- contribs/src/main/java/com/netflix/conductor/contribs/json/JsonJqTransform.java
+++ contribs/src/main/java/com/netflix/conductor/contribs/json/JsonJqTransform.java
@@ -16,6 +16,18 @@
 
 package com.netflix.conductor.contribs.json;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+import javax.annotation.Nonnull;
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.cache.CacheBuilder;
@@ -26,17 +38,8 @@ import com.netflix.conductor.common.run.Workflow;
 import com.netflix.conductor.core.execution.WorkflowExecutor;
 import com.netflix.conductor.core.execution.tasks.WorkflowSystemTask;
 import net.thisptr.jackson.jq.JsonQuery;
+import net.thisptr.jackson.jq.Scope;
 import net.thisptr.jackson.jq.exception.JsonQueryException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import javax.annotation.Nonnull;
-import javax.inject.Inject;
-import javax.inject.Singleton;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.concurrent.TimeUnit;
 
 @Singleton
 public class JsonJqTransform extends WorkflowSystemTask {
@@ -46,7 +49,7 @@ public class JsonJqTransform extends WorkflowSystemTask {
     private static final String OUTPUT_RESULT = "result";
     private static final String OUTPUT_RESULT_LIST = "resultList";
     private static final String OUTPUT_ERROR = "error";
-
+    private final Scope rootScope;
     private final ObjectMapper objectMapper;
     private final LoadingCache<String, JsonQuery> queryCache = createQueryCache();
 
@@ -54,6 +57,8 @@ public class JsonJqTransform extends WorkflowSystemTask {
     public JsonJqTransform(ObjectMapper objectMapper) {
         super(NAME);
         this.objectMapper = objectMapper;
+        this.rootScope = Scope.newEmptyScope();
+        this.rootScope.loadFunctions(Scope.class.getClassLoader());
     }
 
     @Override
@@ -72,7 +77,10 @@ public class JsonJqTransform extends WorkflowSystemTask {
         try {
             final JsonNode input = objectMapper.valueToTree(taskInput);
             final JsonQuery query = queryCache.get(queryExpression);
-            final List<JsonNode> result = query.apply(input);
+
+            final Scope childScope = Scope.newChildScope(rootScope);
+
+            final List<JsonNode> result = query.apply(childScope, input);
 
             task.setStatus(Task.Status.COMPLETED);
             if (result == null) {
