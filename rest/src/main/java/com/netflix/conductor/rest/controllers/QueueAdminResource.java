baf24915da88637192b009d885562b4b038876e5
Anoop Panicker <apanicker@netflix.com>
2020-12-10 12:08:32 -0800

    added queue admin resource

---
+++ rest/src/main/java/com/netflix/conductor/rest/controllers/QueueAdminResource.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2020 Netflix, Inc.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+package com.netflix.conductor.rest.controllers;
+
+import static com.netflix.conductor.rest.config.RequestMappingConstants.QUEUE;
+
+import com.netflix.conductor.common.metadata.tasks.Task.Status;
+import com.netflix.conductor.core.events.queue.QueueManager;
+import io.swagger.v3.oas.annotations.Operation;
+import java.util.Map;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(QUEUE)
+public class QueueAdminResource {
+
+    private final QueueManager queueManager;
+
+    public QueueAdminResource(QueueManager queueManager) {
+        this.queueManager = queueManager;
+    }
+
+    @Operation(summary = "Get the queue length")
+    @GetMapping(value = "/size")
+    public Map<String, Long> size() {
+        return queueManager.size();
+    }
+
+    @Operation(summary = "Get Queue Names")
+    @GetMapping(value = "/")
+    public Map<Status, String> names() {
+        return queueManager.queues();
+    }
+
+    @Operation(summary = "Publish a message in queue to mark a wait task as completed.")
+    @PostMapping(value = "/update/{workflowId}/{taskRefName}/{status}")
+    public void update(@PathVariable("workflowId") String workflowId, @PathVariable("taskRefName") String taskRefName,
+        @PathVariable("status") Status status, Map<String, Object> output) throws Exception {
+        queueManager.updateByTaskRefName(workflowId, taskRefName, output, status);
+    }
+
+    @Operation(summary = "Publish a message in queue to mark a wait task (by taskId) as completed.")
+    @PostMapping("/update/{workflowId}/task/{taskId}/{status}")
+    public void updateByTaskId(@PathVariable("workflowId") String workflowId, @PathVariable("taskId") String taskId,
+        @PathVariable("status") Status status, Map<String, Object> output) throws Exception {
+        queueManager.updateByTaskId(workflowId, taskId, output, status);
+    }
+}
