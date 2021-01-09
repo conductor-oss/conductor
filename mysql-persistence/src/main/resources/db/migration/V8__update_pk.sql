9c52b62592948743da6b49231a9ed0f0114d4840
mactaggart <mactaggart@users.noreply.github.com>
2021-01-09 10:09:15 -0800

    refactor stored procedures for updating PK for mysql

--- mysql-persistence/src/main/resources/db/migration/V8__update_pk.sql
+++ mysql-persistence/src/main/resources/db/migration/V8__update_pk.sql
@@ -3,106 +3,101 @@ DROP PROCEDURE IF EXISTS `DropIndexIfExists`$$
 CREATE PROCEDURE `DropIndexIfExists`(IN tableName VARCHAR(128), IN indexName VARCHAR(128))
 BEGIN
 
-    DECLARE idx_exists INT DEFAULT 0;
+    DECLARE index_exists INT DEFAULT 0;
 
-    SET idx_exists = (SELECT COUNT(*)
-                      FROM information_schema.STATISTICS
-                      WHERE TABLE_NAME = tableName
-                        AND INDEX_NAME = indexName
-                        AND TABLE_SCHEMA = database());
+    SELECT COUNT(1) INTO index_exists
+    FROM INFORMATION_SCHEMA.STATISTICS
+    WHERE TABLE_NAME   = tableName
+      AND INDEX_NAME   = indexName
+      AND TABLE_SCHEMA = database();
 
-    SET @idx_stmt = IF(idx_exists > 0,
-                       CONCAT('ALTER TABLE ', tableName, ' DROP INDEX ', indexName),
-                       'SELECT CONCAT(''INFO: Index "'', indexName, ''" does not exist'')');
+    IF index_exists > 0 THEN
 
-    PREPARE stmt FROM @idx_stmt;
-    EXECUTE stmt;
-    DEALLOCATE PREPARE stmt;
+        SELECT CONCAT('INFO: Dropping Index ', indexName, ' on table ', tableName);
+        SET @stmt = CONCAT('ALTER TABLE ', tableName, ' DROP INDEX ', indexName);
+        PREPARE st FROM @stmt;
+        EXECUTE st;
+        DEALLOCATE PREPARE st;
+
+    ELSE
+        SELECT CONCAT('INFO: Index ', indexName, ' does not exists on table ', tableName);
+    END IF;
+
 END$$
 
-DROP PROCEDURE IF EXISTS `DropColumnIfExists`$$
-CREATE PROCEDURE `DropColumnIfExists`(IN tableName VARCHAR(128), IN columnName VARCHAR(128))
+DROP PROCEDURE IF EXISTS `FixPkIfNeeded`$$
+CREATE PROCEDURE `FixPkIfNeeded`(IN tableName VARCHAR(128), IN columns VARCHAR(128))
 BEGIN
 
     DECLARE col_exists INT DEFAULT 0;
 
-    SET col_exists = (SELECT COUNT(*)
-                      FROM information_schema.COLUMNS
-                      WHERE TABLE_NAME = tableName
-                        AND COLUMN_NAME = columnName
-                        AND TABLE_SCHEMA = database());
+    SELECT COUNT(1) INTO col_exists
+    FROM INFORMATION_SCHEMA.COLUMNS
+    WHERE TABLE_NAME   = tableName
+      AND COLUMN_NAME  = 'id'
+      AND TABLE_SCHEMA = database();
 
-    SET @col_stmt = IF(col_exists > 0,
-                       CONCAT('ALTER TABLE ', tableName, ' DROP COLUMN ', columnName),
-                       'SELECT CONCAT(''INFO: Column "'', columnName,''" does not exist on table "'', tableName, ''"'')');
+    IF col_exists > 0 THEN
 
-    PREPARE stmt FROM @col_stmt;
-    EXECUTE stmt;
-    DEALLOCATE PREPARE stmt;
+        SELECT CONCAT('INFO: Updating PK on table ', tableName);
+
+        SET @stmt = CONCAT('ALTER TABLE ', tableName, ' MODIFY id INT');
+        PREPARE st FROM @stmt;
+        EXECUTE st;
+        DEALLOCATE PREPARE st;
+
+        SET @stmt = CONCAT('ALTER TABLE ', tableName, ' DROP PRIMARY KEY, ADD PRIMARY KEY (', columns, ')');
+        PREPARE st FROM @stmt;
+        EXECUTE st;
+        DEALLOCATE PREPARE st;
+
+        SET @stmt = CONCAT('ALTER TABLE ', tableName, ' DROP COLUMN id');
+        PREPARE st FROM @stmt;
+        EXECUTE st;
+        DEALLOCATE PREPARE st;
+
+    ELSE
+        SELECT CONCAT('INFO: Column id does not exists on table ', tableName);
+    END IF;
+
 END$$
 DELIMITER ;
 
-# 1) queue message
 CALL DropIndexIfExists('queue_message', 'unique_queue_name_message_id');
-CALL DropColumnIfExists('queue_message', 'id');
-ALTER TABLE queue_message ADD PRIMARY KEY (queue_name, message_id);
+CALL FixPkIfNeeded('queue_message','queue_name, message_id');
 
-# 2) queue
 CALL DropIndexIfExists('queue', 'unique_queue_name');
-CALL DropColumnIfExists('queue', 'id');
-ALTER TABLE queue ADD PRIMARY KEY (queue_name);
+CALL FixPkIfNeeded('queue','queue_name');
 
-# 3) workflow_to_task
 CALL DropIndexIfExists('workflow_to_task', 'unique_workflow_to_task_id');
-CALL DropColumnIfExists('workflow_to_task', 'id');
-ALTER TABLE workflow_to_task ADD PRIMARY KEY (workflow_id, task_id);
+CALL FixPkIfNeeded('workflow_to_task', 'workflow_id, task_id');
 
-# 4) workflow_pending
 CALL DropIndexIfExists('workflow_pending', 'unique_workflow_type_workflow_id');
-CALL DropColumnIfExists('workflow_pending', 'id');
-ALTER TABLE workflow_pending ADD PRIMARY KEY (workflow_type, workflow_id);
+CALL FixPkIfNeeded('workflow_pending', 'workflow_type, workflow_id');
 
-# 5) workflow_def_to_workflow
 CALL DropIndexIfExists('workflow_def_to_workflow', 'unique_workflow_def_date_str');
-CALL DropColumnIfExists('workflow_def_to_workflow', 'id');
-ALTER TABLE workflow_def_to_workflow ADD PRIMARY KEY (workflow_def, date_str, workflow_id);
+CALL FixPkIfNeeded('workflow_def_to_workflow', 'workflow_def, date_str, workflow_id');
 
-# 6) workflow
 CALL DropIndexIfExists('workflow', 'unique_workflow_id');
-CALL DropColumnIfExists('workflow', 'id');
-ALTER TABLE workflow ADD PRIMARY KEY (workflow_id);
+CALL FixPkIfNeeded('workflow', 'workflow_id');
 
-# 7) task
 CALL DropIndexIfExists('task', 'unique_task_id');
-CALL DropColumnIfExists('task', 'id');
-ALTER TABLE task ADD PRIMARY KEY (task_id);
+CALL FixPkIfNeeded('task', 'task_id');
 
-# 8) task_in_progress
 CALL DropIndexIfExists('task_in_progress', 'unique_task_def_task_id1');
-CALL DropColumnIfExists('task_in_progress', 'id');
-ALTER TABLE task_in_progress ADD PRIMARY KEY (task_def_name, task_id);
+CALL FixPkIfNeeded('task_in_progress', 'task_def_name, task_id');
 
-# 9) task_scheduled
 CALL DropIndexIfExists('task_scheduled', 'unique_workflow_id_task_key');
-CALL DropColumnIfExists('task_scheduled', 'id');
-ALTER TABLE task_scheduled ADD PRIMARY KEY (workflow_id, task_key);
+CALL FixPkIfNeeded('task_scheduled', 'workflow_id, task_key');
 
-# 10) poll_data
 CALL DropIndexIfExists('poll_data', 'unique_poll_data');
-CALL DropColumnIfExists('poll_data', 'id');
-ALTER TABLE poll_data ADD PRIMARY KEY (queue_name, domain);
+CALL FixPkIfNeeded('poll_data','queue_name, domain');
 
-# 11) event_execution
 CALL DropIndexIfExists('event_execution', 'unique_event_execution');
-CALL DropColumnIfExists('event_execution', 'id');
-ALTER TABLE event_execution ADD PRIMARY KEY (event_handler_name, event_name, execution_id);
+CALL FixPkIfNeeded('event_execution', 'event_handler_name, event_name, execution_id');
 
-# 12) meta_workflow_def
 CALL DropIndexIfExists('meta_workflow_def', 'unique_name_version');
-CALL DropColumnIfExists('meta_workflow_def', 'id');
-ALTER TABLE meta_workflow_def ADD PRIMARY KEY (name, version);
+CALL FixPkIfNeeded('meta_workflow_def', 'name, version');
 
-# 13) meta_task_def
 CALL DropIndexIfExists('meta_task_def', 'unique_task_def_name');
-CALL DropColumnIfExists('meta_task_def', 'id');
-ALTER TABLE meta_task_def ADD PRIMARY KEY (name);
+CALL FixPkIfNeeded('meta_task_def','name');
