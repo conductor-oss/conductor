664754eb3937ce6ad87f59a50226528a99b54d8e
Anoop Panicker <apanicker@netflix.com>
2021-04-15 23:22:35 -0700

    removed unused travis files

--- installViaTravis.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# This script will build the project.
-
-if [ "$TRAVIS_SECURE_ENV_VARS" = "true" ]; then
-  echo "Decrypting publishing credentials"
-  openssl aes-256-cbc -k "$NETFLIX_OSS_SIGNING_FILE_PASSWORD" -in secrets/signing-key.enc -out secrets/signing-key -d
-fi
