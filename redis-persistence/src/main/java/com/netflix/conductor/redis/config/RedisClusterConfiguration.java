ecb3b3aa2c8bb222d9ce59d229de99afdb8c8a43
Jack Stenglein <jackstenglein@gmail.com>
2021-05-14 18:55:32 -0500

    Get all IP addresses for redis cluster (#2249)
    * Get all IP addresses for redis cluster
    
    * Use streams API

--- redis-persistence/src/main/java/com/netflix/conductor/redis/config/RedisClusterConfiguration.java
+++ redis-persistence/src/main/java/com/netflix/conductor/redis/config/RedisClusterConfiguration.java
@@ -17,6 +17,8 @@ import com.netflix.conductor.redis.jedis.JedisCluster;
 import com.netflix.dyno.connectionpool.Host;
 import com.netflix.dyno.connectionpool.HostSupplier;
 import com.netflix.dyno.connectionpool.TokenMapSupplier;
+import java.util.stream.Collectors;
+import java.util.Set;
 import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Configuration;
@@ -32,9 +34,9 @@ public class RedisClusterConfiguration extends JedisCommandsConfigurer {
         HostSupplier hostSupplier, TokenMapSupplier tokenMapSupplier) {
         GenericObjectPoolConfig genericObjectPoolConfig = new GenericObjectPoolConfig();
         genericObjectPoolConfig.setMaxTotal(properties.getMaxConnectionsPerHost());
-        Host host = hostSupplier.getHosts().get(0);
-        return new JedisCluster(
-            new redis.clients.jedis.JedisCluster(new HostAndPort(host.getHostName(), host.getPort()),
-                genericObjectPoolConfig));
+        Set hosts = hostSupplier.getHosts().stream()
+            .map(h -> new HostAndPort(h.getHostName(), h.getPort()))
+            .collect(Collectors.toSet());
+        return new JedisCluster(new redis.clients.jedis.JedisCluster(hosts, genericObjectPoolConfig));
     }
 }
