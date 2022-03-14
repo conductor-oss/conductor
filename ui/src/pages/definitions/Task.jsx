1765a287a2746069770ad83067cc86ed64f6dfb4
peterlau <petercslau@gmail.com>
2022-03-14 16:26:02 -0700

    Squashed commit of the following: Separate colors for in_progress and canceled tasks Additional columns in Task Defintions list

--- ui/src/pages/definitions/Task.jsx
+++ ui/src/pages/definitions/Task.jsx
@@ -41,6 +41,8 @@ const columns = [
     sortable: false,
     searchable: false,
   },
+  { name: "concurrentExecLimit" },
+  { name: "pollTimeoutSeconds" },
 ];
 
 export default function TaskDefinitions() {
