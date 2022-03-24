0aa6af7115e21b8bba9da83b9e834582687f51f0
peterlau <petercslau@gmail.com>
2022-03-23 23:54:09 -0700

    [UI] Fix event handler json display

--- ui/src/pages/definition/EventHandler.jsx
+++ ui/src/pages/definition/EventHandler.jsx
@@ -3,10 +3,23 @@ import { useRouteMatch } from "react-router-dom";
 import sharedStyles from "../styles";
 import { makeStyles } from "@material-ui/styles";
 import { Helmet } from "react-helmet";
-import { ReactJson, LinearProgress, Heading } from "../../components";
+import { ReactJson, LinearProgress, Heading, Paper } from "../../components";
 import { useEventHandlers } from "../../data/misc";
 
-const useStyles = makeStyles(sharedStyles);
+const useStyles = makeStyles({
+  wrapper: {
+    display: "flex",
+    height: "100%",
+    alignItems: "stretch",
+    flexDirection: "column",
+  },
+  header: sharedStyles.header,
+  paper: {
+    flex: 1,
+    margin: 30,
+    paddingTop: 10
+  }
+});
 
 export default function EventHandlerDefinition() {
   const classes = useStyles();
@@ -32,9 +45,9 @@ export default function EventHandlerDefinition() {
         <Heading level={4}>{match.params.name}</Heading>
       </div>
       {isFetching && <LinearProgress />}
-      <div className={classes.tabContent}>
+      <Paper className={classes.paper}>
         {eventHandler && <ReactJson src={eventHandler} />}
-      </div>
+      </Paper>
     </div>
   );
 }
