36a62d20ee40bd9454c5bb5097839911403a2f8b
Viren Baraiya <virenx@gmail.com>
2022-05-31 15:08:50 -0700

    Human Task (#2987)

--- common/src/main/java/com/netflix/conductor/common/metadata/tasks/TaskType.java
+++ common/src/main/java/com/netflix/conductor/common/metadata/tasks/TaskType.java
@@ -31,6 +31,7 @@ public enum TaskType {
     START_WORKFLOW,
     EVENT,
     WAIT,
+    HUMAN,
     USER_DEFINED,
     HTTP,
     LAMBDA,
@@ -54,6 +55,7 @@ public enum TaskType {
     public static final String TASK_TYPE_FORK_JOIN_DYNAMIC = "FORK_JOIN_DYNAMIC";
     public static final String TASK_TYPE_EVENT = "EVENT";
     public static final String TASK_TYPE_WAIT = "WAIT";
+    public static final String TASK_TYPE_HUMAN = "HUMAN";
     public static final String TASK_TYPE_SUB_WORKFLOW = "SUB_WORKFLOW";
     public static final String TASK_TYPE_START_WORKFLOW = "START_WORKFLOW";
     public static final String TASK_TYPE_FORK_JOIN = "FORK_JOIN";
