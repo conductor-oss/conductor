d013dcd78416a87234404c4b105adface948bea6
peterlau <petercslau@gmail.com>
2022-06-08 11:21:36 -0700

    [UI] Use new logo in AppLogo

--- ui/src/plugins/AppLogo.jsx
+++ ui/src/plugins/AppLogo.jsx
@@ -3,12 +3,13 @@ import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
   logo: {
-    height: 55,
+    height: 37,
+    width: 175,
     marginRight: 30,
   },
 }));
 
 export default function AppLogo() {
   const classes = useStyles();
-  return <img src="/logo.png" alt="Conductor" className={classes.logo} />;
+  return <img src="/logo.svg" alt="Conductor" className={classes.logo} />;
 }
