8ca3b96f401ded8b94dea47688be98d9d23f4ace
peterlau <petercslau@gmail.com>
2022-08-12 14:23:23 -0700

    Export handleError

--- ui/src/export.js
+++ ui/src/export.js
@@ -1,5 +1,6 @@
 import App from "./App";
 import AppContext from "./components/context/AppContext";
+import handleError from "./utils/handleError";
 
 export * from "./components";
-export { App, AppContext };
+export { App, AppContext, handleError };
