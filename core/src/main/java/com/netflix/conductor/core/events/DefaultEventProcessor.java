e02b1fa3e737adf86f91281e91494e1931809c1b
Avital Ofstein <avitalof@payoneer.com>
2022-12-01 17:53:48 +0200

    Adding possibility to Nack messages when processing events from queue - supporting cases of transient failures, that messages we can't process- won't be lost by ack, and not re-published back to the queue, but published to DLQ (in some queue implementation).

--- core/src/main/java/com/netflix/conductor/core/events/DefaultEventProcessor.java
+++ core/src/main/java/com/netflix/conductor/core/events/DefaultEventProcessor.java
@@ -134,6 +134,10 @@ public class DefaultEventProcessor {
                 queue.publish(Collections.singletonList(msg));
                 LOGGER.debug("Message: {} published to queue: {}", msg.getId(), queue.getName());
             }
+            else {
+                queue.nack(Collections.singletonList(msg));
+                LOGGER.debug("Message: {} nacked on queue: {}", msg.getId(), queue.getName());
+            }
             Monitors.recordEventQueueMessagesHandled(queue.getType(), queue.getName());
         }
     }
