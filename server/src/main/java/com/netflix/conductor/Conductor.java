5a0dcc1e5def3253f4560baa89828976514c1cc3
manan164 <manan16489@gmail.com>
2023-01-10 19:03:00 +0530

    Make orkes queue default
    Deprecate dyno queue.

--- server/src/main/java/com/netflix/conductor/Conductor.java
+++ server/src/main/java/com/netflix/conductor/Conductor.java
@@ -21,12 +21,14 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.io.FileSystemResource;
 
 // Prevents from the datasource beans to be loaded, AS they are needed only for specific databases.
 // In case that SQL database is selected this class will be imported back in the appropriate
 // database persistence module.
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@ComponentScan(basePackages = {"com.netflix.conductor", "io.orkes.conductor"})
 public class Conductor {
 
     private static final Logger log = LoggerFactory.getLogger(Conductor.class);
