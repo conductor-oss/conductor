878a5128c2c6606393f33bbb64947ff8192f9d1a
saksham2105 <solankisaksham1998@gmail.com>
2023-09-02 03:30:27 +0530

    issue-3719: Fix for http task stuck due to NPE

--- core/src/main/java/com/netflix/conductor/core/storage/DummyPayloadStorage.java
+++ core/src/main/java/com/netflix/conductor/core/storage/DummyPayloadStorage.java
@@ -12,10 +12,15 @@
  */
 package com.netflix.conductor.core.storage;
 
+import java.io.ByteArrayInputStream;
 import java.io.InputStream;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 import com.netflix.conductor.common.run.ExternalStorageLocation;
 import com.netflix.conductor.common.utils.ExternalPayloadStorage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * A dummy implementation of {@link ExternalPayloadStorage} used when no external payload is
@@ -23,17 +28,41 @@ import com.netflix.conductor.common.utils.ExternalPayloadStorage;
  */
 public class DummyPayloadStorage implements ExternalPayloadStorage {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(DummyPayloadStorage.class);
+
+    private final Map<String, byte[]> dummyDataStore = new ConcurrentHashMap<>();
+
+    private static final String DUMMY_DATA_STORE_KEY = "DUMMY_PAYLOAD_STORE_KEY";
+
     @Override
     public ExternalStorageLocation getLocation(
             Operation operation, PayloadType payloadType, String path) {
-        return null;
+        ExternalStorageLocation externalStorageLocation = new ExternalStorageLocation();
+        externalStorageLocation.setPath(path != null ? path : "");
+        return externalStorageLocation;
     }
 
     @Override
-    public void upload(String path, InputStream payload, long payloadSize) {}
+    public void upload(String path, InputStream payload, long payloadSize) {
+        try {
+            final byte[] payloadBytes = new byte[(int) payloadSize];
+            final int bytesRead = payload.read(new byte[(int) payloadSize]);
+
+            if (bytesRead > 0) {
+                dummyDataStore.put(path == null || path.isEmpty() ? DUMMY_DATA_STORE_KEY : path, payloadBytes);
+            }
+        } catch (Exception e) {
+            LOGGER.error("Error encountered while uploading payload {}", e.getMessage());
+        }
+    }
 
     @Override
     public InputStream download(String path) {
-        return null;
+        final byte[] data = dummyDataStore.get(path == null || path.isEmpty() ? DUMMY_DATA_STORE_KEY : path);
+        if (data != null) {
+            return new ByteArrayInputStream(data);
+        } else {
+            return null;
+        }
     }
 }
