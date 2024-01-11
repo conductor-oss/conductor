fa097a7c9bc6fe654e89323938ed2a0c6636d703
Dennis Caldwell <dcaldwell@callibrity.com>
2024-01-11 13:50:50 -0500

    Update ElasticSearch to 7.17.16, some sdk tests are failing.

--- es7-persistence/src/test/java/com/netflix/conductor/es7/dao/index/ElasticSearchTest.java
+++ es7-persistence/src/test/java/com/netflix/conductor/es7/dao/index/ElasticSearchTest.java
@@ -47,8 +47,8 @@ public abstract class ElasticSearchTest {
 
     protected static final ElasticsearchContainer container =
             new ElasticsearchContainer(
-                    DockerImageName.parse("docker.elastic.co/elasticsearch/elasticsearch-oss")
-                            .withTag("7.10.2")); // this should match the client version
+                    DockerImageName.parse("elasticsearch")
+                            .withTag("7.17.16")); // this should match the client version
 
     @Autowired protected ObjectMapper objectMapper;
 
--- test-harness/src/test/java/com/netflix/conductor/test/integration/AbstractEndToEndTest.java
+++ test-harness/src/test/java/com/netflix/conductor/test/integration/AbstractEndToEndTest.java
@@ -65,8 +65,8 @@ public abstract class AbstractEndToEndTest {
 
     private static final ElasticsearchContainer container =
             new ElasticsearchContainer(
-                    DockerImageName.parse("docker.elastic.co/elasticsearch/elasticsearch-oss")
-                            .withTag("7.10.2")); // this should match the client version
+                    DockerImageName.parse("elasticsearch")
+                            .withTag("7.17.16")); // this should match the client version
 
     private static RestClient restClient;
 
