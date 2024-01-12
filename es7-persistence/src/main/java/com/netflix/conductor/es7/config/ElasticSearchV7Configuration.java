2a72da5c5a181f15877dfbf6b539d2f50eab3b78
Dennis Caldwell <dcaldwell@callibrity.com>
2024-01-12 11:20:17 -0500

    Update Spring Boot to 3.2.1. One failing test in end to end.

--- es7-persistence/src/main/java/com/netflix/conductor/es7/config/ElasticSearchV7Configuration.java
+++ es7-persistence/src/main/java/com/netflix/conductor/es7/config/ElasticSearchV7Configuration.java
@@ -59,7 +59,7 @@ public class ElasticSearchV7Configuration {
     }
 
     @Bean
-    public RestClientBuilder restClientBuilder(ElasticSearchProperties properties) {
+    public RestClientBuilder elasticRestClientBuilder(ElasticSearchProperties properties) {
         RestClientBuilder builder = RestClient.builder(convertToHttpHosts(properties.toURLs()));
 
         if (properties.getUsername() != null && properties.getPassword() != null) {
