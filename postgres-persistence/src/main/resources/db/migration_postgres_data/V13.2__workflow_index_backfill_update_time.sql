ca60fcadf89d0814e47cb40e9cda588e6b35b9e2
Lisa Best <lbest@atlassian.com>
2024-10-18 11:59:37 +1030

    Amend the backfill script as further testing detected it was not handling hours correctly or interpreting the Z timezone as UTC.

--- postgres-persistence/src/main/resources/db/migration_postgres_data/V13.2__workflow_index_backfill_update_time.sql
+++ postgres-persistence/src/main/resources/db/migration_postgres_data/V13.2__workflow_index_backfill_update_time.sql
@@ -1,4 +1,4 @@
 -- Optional back-fill script to populate updateTime historically.
 UPDATE workflow_index
-SET update_time = to_timestamp(json_data->>'updateTime', 'YYYY-MM-DDTHH24:MI:SS.MSZ')::timestamp WITH time zone
+SET update_time = to_timestamp(json_data->>'updateTime', 'YYYY-MM-DD"T"HH24:MI:SS.MS')::timestamp AT TIME ZONE '00:00'
 WHERE json_data->>'updateTime' IS NOT NULL;
