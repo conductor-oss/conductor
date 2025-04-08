a16313a0898ad98798b9309924adce92dbb5ac81
orkes-harshil <harshil.raval@orkes.io>
2025-04-08 15:13:06 +0530

    Update SDK POJO () to match server implementation

--- conductor-clients/java/conductor-java-sdk/conductor-client/src/main/java/com/netflix/conductor/common/metadata/workflow/StartWorkflowRequest.java
+++ conductor-clients/java/conductor-java-sdk/conductor-client/src/main/java/com/netflix/conductor/common/metadata/workflow/StartWorkflowRequest.java
@@ -15,6 +15,12 @@ package com.netflix.conductor.common.metadata.workflow;
 import java.util.HashMap;
 import java.util.Map;
 
+import lombok.*;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class StartWorkflowRequest {
 
     private String name;
@@ -38,137 +44,4 @@ public class StartWorkflowRequest {
     private String idempotencyKey;
 
     private IdempotencyStrategy idempotencyStrategy;
-
-    public String getIdempotencyKey() {
-        return idempotencyKey;
-    }
-
-    public void setIdempotencyKey(String idempotencyKey) {
-        this.idempotencyKey = idempotencyKey;
-    }
-
-    public IdempotencyStrategy getIdempotencyStrategy() {
-        return idempotencyStrategy;
-    }
-
-    public void setIdempotencyStrategy(IdempotencyStrategy idempotencyStrategy) {
-        this.idempotencyStrategy = idempotencyStrategy;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public StartWorkflowRequest withName(String name) {
-        this.name = name;
-        return this;
-    }
-
-    public Integer getVersion() {
-        return version;
-    }
-
-    public void setVersion(Integer version) {
-        this.version = version;
-    }
-
-    public StartWorkflowRequest withVersion(Integer version) {
-        this.version = version;
-        return this;
-    }
-
-    public String getCorrelationId() {
-        return correlationId;
-    }
-
-    public void setCorrelationId(String correlationId) {
-        this.correlationId = correlationId;
-    }
-
-    public StartWorkflowRequest withCorrelationId(String correlationId) {
-        this.correlationId = correlationId;
-        return this;
-    }
-
-    public String getExternalInputPayloadStoragePath() {
-        return externalInputPayloadStoragePath;
-    }
-
-    public void setExternalInputPayloadStoragePath(String externalInputPayloadStoragePath) {
-        this.externalInputPayloadStoragePath = externalInputPayloadStoragePath;
-    }
-
-    public StartWorkflowRequest withExternalInputPayloadStoragePath(String externalInputPayloadStoragePath) {
-        this.externalInputPayloadStoragePath = externalInputPayloadStoragePath;
-        return this;
-    }
-
-    public Integer getPriority() {
-        return priority;
-    }
-
-    public void setPriority(Integer priority) {
-        this.priority = priority;
-    }
-
-    public StartWorkflowRequest withPriority(Integer priority) {
-        this.priority = priority;
-        return this;
-    }
-
-    public Map<String, Object> getInput() {
-        return input;
-    }
-
-    public void setInput(Map<String, Object> input) {
-        this.input = input;
-    }
-
-    public StartWorkflowRequest withInput(Map<String, Object> input) {
-        this.input = input;
-        return this;
-    }
-
-    public Map<String, String> getTaskToDomain() {
-        return taskToDomain;
-    }
-
-    public void setTaskToDomain(Map<String, String> taskToDomain) {
-        this.taskToDomain = taskToDomain;
-    }
-
-    public StartWorkflowRequest withTaskToDomain(Map<String, String> taskToDomain) {
-        this.taskToDomain = taskToDomain;
-        return this;
-    }
-
-    public WorkflowDef getWorkflowDef() {
-        return workflowDef;
-    }
-
-    public void setWorkflowDef(WorkflowDef workflowDef) {
-        this.workflowDef = workflowDef;
-    }
-
-    public StartWorkflowRequest withWorkflowDef(WorkflowDef workflowDef) {
-        this.workflowDef = workflowDef;
-        return this;
-    }
-
-    public String getCreatedBy() {
-        return createdBy;
-    }
-
-    public void setCreatedBy(String createdBy) {
-        this.createdBy = createdBy;
-    }
-
-    public StartWorkflowRequest withCreatedBy(String createdBy) {
-        this.createdBy = createdBy;
-        return this;
-    }
 }
