0cd0baf79325d114c24658a6f30660a95fae635d
orkes-harshil <harshil.raval@orkes.io>
2025-04-08 15:31:14 +0530

    Update SDK POJO () to match server implementation

--- conductor-clients/java/conductor-java-sdk/orkes-client/src/main/java/io/orkes/conductor/client/model/integration/ai/StoreEmbeddingsInput.java
+++ conductor-clients/java/conductor-java-sdk/orkes-client/src/main/java/io/orkes/conductor/client/model/integration/ai/StoreEmbeddingsInput.java
@@ -15,11 +15,12 @@ package io.orkes.conductor.client.model.integration.ai;
 import java.util.List;
 import java.util.Map;
 
-import lombok.Data;
-import lombok.EqualsAndHashCode;
+import lombok.*;
 
 @Data
 @EqualsAndHashCode(callSuper = true)
+@NoArgsConstructor
+@AllArgsConstructor
 public class StoreEmbeddingsInput extends LLMWorkerInput {
 
     private String vectorDB;
