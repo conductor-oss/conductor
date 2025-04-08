eb4d917068545c3a31b2f3c80ca9af830563973b
orkes-harshil <harshil.raval@orkes.io>
2025-04-08 15:31:53 +0530

    Update SDK POJO () to match server implementation

--- conductor-clients/java/conductor-java-sdk/orkes-client/src/main/java/io/orkes/conductor/client/model/ConductorUser.java
+++ conductor-clients/java/conductor-java-sdk/orkes-client/src/main/java/io/orkes/conductor/client/model/ConductorUser.java
@@ -15,11 +15,14 @@ package io.orkes.conductor.client.model;
 import java.util.ArrayList;
 import java.util.List;
 
-import lombok.EqualsAndHashCode;
-import lombok.ToString;
+import lombok.*;
 
 @EqualsAndHashCode
 @ToString
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ConductorUser {
 
     private Boolean applicationUser = null;
@@ -34,68 +37,11 @@ public class ConductorUser {
 
     private String uuid = null;
 
-    public ConductorUser applicationUser(Boolean applicationUser) {
-        this.applicationUser = applicationUser;
-        return this;
-    }
-
-    public Boolean isApplicationUser() {
-        return applicationUser;
-    }
-
-    public void setApplicationUser(Boolean applicationUser) {
-        this.applicationUser = applicationUser;
-    }
-
-    public ConductorUser groups(List<Group> groups) {
-        this.groups = groups;
-        return this;
-    }
-
     public ConductorUser addGroupsItem(Group groupsItem) {
         if (this.groups == null) {
             this.groups = new ArrayList<>();
         }
         this.groups.add(groupsItem);
-        return this;
-    }
-
-    public List<Group> getGroups() {
-        return groups;
-    }
-
-    public void setGroups(List<Group> groups) {
-        this.groups = groups;
-    }
-
-    public ConductorUser id(String id) {
-        this.id = id;
-        return this;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public ConductorUser name(String name) {
-        this.name = name;
-        return this;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public ConductorUser roles(List<Role> roles) {
-        this.roles = roles;
         return this;
     }
 
@@ -105,26 +51,5 @@ public class ConductorUser {
         }
         this.roles.add(rolesItem);
         return this;
-    }
-
-    public List<Role> getRoles() {
-        return roles;
-    }
-
-    public void setRoles(List<Role> roles) {
-        this.roles = roles;
-    }
-
-    public ConductorUser uuid(String uuid) {
-        this.uuid = uuid;
-        return this;
-    }
-
-    public String getUuid() {
-        return uuid;
-    }
-
-    public void setUuid(String uuid) {
-        this.uuid = uuid;
     }
 }
