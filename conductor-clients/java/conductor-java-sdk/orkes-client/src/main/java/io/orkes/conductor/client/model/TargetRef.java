6ee353d07a2d1d0bff087d9174f240514033f0f2
orkes-harshil <harshil.raval@orkes.io>
2025-04-08 12:09:08 +0530

    Update SDK POJO () to match server implementation

--- conductor-clients/java/conductor-java-sdk/orkes-client/src/main/java/io/orkes/conductor/client/model/TargetRef.java
+++ conductor-clients/java/conductor-java-sdk/orkes-client/src/main/java/io/orkes/conductor/client/model/TargetRef.java
@@ -1,3 +1,4 @@
+```
 /*
  * Copyright 2022 Conductor Authors.
  * <p>
@@ -12,14 +13,14 @@
  */
 package io.orkes.conductor.client.model;
 
-import lombok.EqualsAndHashCode;
-import lombok.ToString;
+import lombok.*;
 
-/**
- * The object over which access is being granted or removed
- */
 @EqualsAndHashCode
 @ToString
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class TargetRef {
 
     private String id = null;
@@ -58,7 +59,6 @@ public class TargetRef {
         }
     }
 
-
     private TypeEnum type = null;
 
     public TargetRef id(String id) {
@@ -66,36 +66,9 @@ public class TargetRef {
         return this;
     }
 
-    /**
-     * Get id
-     * 
-     * @return id
-     **/
-    
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
     public TargetRef type(TypeEnum type) {
         this.type = type;
         return this;
     }
-
-    /**
-     * Get type
-     * 
-     * @return type
-     **/
-    
-    public TypeEnum getType() {
-        return type;
-    }
-
-    public void setType(TypeEnum type) {
-        this.type = type;
-    }
 }
+```
