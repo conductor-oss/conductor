1859b4f4c346c0800d19ee8bb6769391c44deec1
orkes-harshil <harshil.raval@orkes.io>
2025-04-08 14:16:58 +0530

    Update SDK POJO () to match server implementation

--- conductor-clients/java/conductor-java-sdk/orkes-client/src/main/java/io/orkes/conductor/client/model/Subject.java
+++ conductor-clients/java/conductor-java-sdk/orkes-client/src/main/java/io/orkes/conductor/client/model/Subject.java
@@ -12,11 +12,14 @@
  */
 package io.orkes.conductor.client.model;
 
-import lombok.EqualsAndHashCode;
-import lombok.ToString;
+import lombok.*;
 
 @EqualsAndHashCode
 @ToString
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Subject {
 
     private String id = null;
@@ -53,30 +56,4 @@ public class Subject {
     }
 
     private TypeEnum type = null;
-
-    public Subject id(String id) {
-        this.id = id;
-        return this;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public Subject type(TypeEnum type) {
-        this.type = type;
-        return this;
-    }
-
-    public TypeEnum getType() {
-        return type;
-    }
-
-    public void setType(TypeEnum type) {
-        this.type = type;
-    }
 }
