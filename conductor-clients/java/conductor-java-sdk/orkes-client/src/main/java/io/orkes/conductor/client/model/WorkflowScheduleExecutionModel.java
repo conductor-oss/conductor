ef7d11c5bfe7862353da7e5c1d5caf088347c25f
orkes-harshil <harshil.raval@orkes.io>
2025-04-08 15:33:25 +0530

    Update SDK POJO () to match server implementation

--- conductor-clients/java/conductor-java-sdk/orkes-client/src/main/java/io/orkes/conductor/client/model/WorkflowScheduleExecutionModel.java
+++ conductor-clients/java/conductor-java-sdk/orkes-client/src/main/java/io/orkes/conductor/client/model/WorkflowScheduleExecutionModel.java
@@ -14,11 +14,14 @@ package io.orkes.conductor.client.model;
 
 import com.netflix.conductor.common.metadata.workflow.StartWorkflowRequest;
 
-import lombok.EqualsAndHashCode;
-import lombok.ToString;
+import lombok.*;
 
 @EqualsAndHashCode
 @ToString
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class WorkflowScheduleExecutionModel {
 
     private String executionId = null;
@@ -71,134 +74,11 @@ public class WorkflowScheduleExecutionModel {
 
     private String workflowName = null;
 
-    public WorkflowScheduleExecutionModel executionId(String executionId) {
-        this.executionId = executionId;
+    public WorkflowScheduleExecutionModel addSampleRefItem(SampleRef sampleRefItem) {
+        if (this.sampleRef == null) {
+            this.sampleRef = new ArrayList<>();
+        }
+        this.sampleRef.add(sampleRefItem);
         return this;
-    }
-
-    public String getExecutionId() {
-        return executionId;
-    }
-
-    public void setExecutionId(String executionId) {
-        this.executionId = executionId;
-    }
-
-    public WorkflowScheduleExecutionModel executionTime(Long executionTime) {
-        this.executionTime = executionTime;
-        return this;
-    }
-
-    public Long getExecutionTime() {
-        return executionTime;
-    }
-
-    public void setExecutionTime(Long executionTime) {
-        this.executionTime = executionTime;
-    }
-
-    public WorkflowScheduleExecutionModel reason(String reason) {
-        this.reason = reason;
-        return this;
-    }
-
-    public String getReason() {
-        return reason;
-    }
-
-    public void setReason(String reason) {
-        this.reason = reason;
-    }
-
-    public WorkflowScheduleExecutionModel scheduleName(String scheduleName) {
-        this.scheduleName = scheduleName;
-        return this;
-    }
-
-    public String getScheduleName() {
-        return scheduleName;
-    }
-
-    public void setScheduleName(String scheduleName) {
-        this.scheduleName = scheduleName;
-    }
-
-    public WorkflowScheduleExecutionModel scheduledTime(Long scheduledTime) {
-        this.scheduledTime = scheduledTime;
-        return this;
-    }
-
-    public Long getScheduledTime() {
-        return scheduledTime;
-    }
-
-    public void setScheduledTime(Long scheduledTime) {
-        this.scheduledTime = scheduledTime;
-    }
-
-    public WorkflowScheduleExecutionModel stackTrace(String stackTrace) {
-        this.stackTrace = stackTrace;
-        return this;
-    }
-
-    public String getStackTrace() {
-        return stackTrace;
-    }
-
-    public void setStackTrace(String stackTrace) {
-        this.stackTrace = stackTrace;
-    }
-
-    public WorkflowScheduleExecutionModel startWorkflowRequest(
-            StartWorkflowRequest startWorkflowRequest) {
-        this.startWorkflowRequest = startWorkflowRequest;
-        return this;
-    }
-
-    public StartWorkflowRequest getStartWorkflowRequest() {
-        return startWorkflowRequest;
-    }
-
-    public void setStartWorkflowRequest(StartWorkflowRequest startWorkflowRequest) {
-        this.startWorkflowRequest = startWorkflowRequest;
-    }
-
-    public WorkflowScheduleExecutionModel state(StateEnum state) {
-        this.state = state;
-        return this;
-    }
-
-    public StateEnum getState() {
-        return state;
-    }
-
-    public void setState(StateEnum state) {
-        this.state = state;
-    }
-
-    public WorkflowScheduleExecutionModel workflowId(String workflowId) {
-        this.workflowId = workflowId;
-        return this;
-    }
-
-    public String getWorkflowId() {
-        return workflowId;
-    }
-
-    public void setWorkflowId(String workflowId) {
-        this.workflowId = workflowId;
-    }
-
-    public WorkflowScheduleExecutionModel workflowName(String workflowName) {
-        this.workflowName = workflowName;
-        return this;
-    }
-
-    public String getWorkflowName() {
-        return workflowName;
-    }
-
-    public void setWorkflowName(String workflowName) {
-        this.workflowName = workflowName;
     }
 }
