47b10b3e13d5f0684d09f5de30394707aeffbd86
orkes-harshil <harshil.raval@orkes.io>
2025-04-08 18:53:00 +0530

    Update SDK POJO () to match server implementation

--- conductor-clients/java/conductor-java-sdk/conductor-client/src/main/java/com/netflix/conductor/common/metadata/workflow/DynamicForkJoinTaskList.java
+++ conductor-clients/java/conductor-java-sdk/conductor-client/src/main/java/com/netflix/conductor/common/metadata/workflow/DynamicForkJoinTaskList.java
@@ -1,4 +1,4 @@
-/*
+/* 
  * Copyright 2020 Conductor Authors.
  * <p>
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
@@ -16,8 +16,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-import com.netflix.conductor.common.metadata.tasks.TaskType;
-
 import lombok.*;
 
 @Data
@@ -29,10 +27,18 @@ public class DynamicForkJoinTaskList {
     private List<DynamicForkJoinTask> dynamicTasks = new ArrayList<>();
 
     public void add(String taskName, String workflowName, String referenceName, Map<String, Object> input) {
-        dynamicTasks.add(new DynamicForkJoinTask(taskName, workflowName, referenceName, input, TaskType.SIMPLE.name()));
+        dynamicTasks.add(new DynamicForkJoinTask(taskName, workflowName, referenceName, input));
     }
 
     public void add(DynamicForkJoinTask dtask) {
         dynamicTasks.add(dtask);
+    }
+
+    public List<DynamicForkJoinTask> getDynamicTasks() {
+        return dynamicTasks;
+    }
+
+    public void setDynamicTasks(List<DynamicForkJoinTask> dynamicTasks) {
+        this.dynamicTasks = dynamicTasks;
     }
 }
