a2741c5d24eb719ce2ccf4d8a94f17460f2d4483
orkes-harshil <harshil.raval@orkes.io>
2025-04-08 15:35:15 +0530

    Update SDK POJO () to match server implementation

--- conductor-clients/java/conductor-java-sdk/conductor-client/src/main/java/com/netflix/conductor/common/metadata/workflow/DynamicForkJoinTask.java
+++ conductor-clients/java/conductor-java-sdk/conductor-client/src/main/java/com/netflix/conductor/common/metadata/workflow/DynamicForkJoinTask.java
@@ -17,6 +17,12 @@ import java.util.Map;
 
 import com.netflix.conductor.common.metadata.tasks.TaskType;
 
+import lombok.*;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class DynamicForkJoinTask {
 
     private String taskName;
@@ -28,64 +34,4 @@ public class DynamicForkJoinTask {
     private Map<String, Object> input = new HashMap<>();
 
     private String type = TaskType.SIMPLE.name();
-
-    public DynamicForkJoinTask() {
-    }
-
-    public DynamicForkJoinTask(String taskName, String workflowName, String referenceName, Map<String, Object> input) {
-        super();
-        this.taskName = taskName;
-        this.workflowName = workflowName;
-        this.referenceName = referenceName;
-        this.input = input;
-    }
-
-    public DynamicForkJoinTask(String taskName, String workflowName, String referenceName, String type, Map<String, Object> input) {
-        super();
-        this.taskName = taskName;
-        this.workflowName = workflowName;
-        this.referenceName = referenceName;
-        this.input = input;
-        this.type = type;
-    }
-
-    public String getTaskName() {
-        return taskName;
-    }
-
-    public void setTaskName(String taskName) {
-        this.taskName = taskName;
-    }
-
-    public String getWorkflowName() {
-        return workflowName;
-    }
-
-    public void setWorkflowName(String workflowName) {
-        this.workflowName = workflowName;
-    }
-
-    public String getReferenceName() {
-        return referenceName;
-    }
-
-    public void setReferenceName(String referenceName) {
-        this.referenceName = referenceName;
-    }
-
-    public Map<String, Object> getInput() {
-        return input;
-    }
-
-    public void setInput(Map<String, Object> input) {
-        this.input = input;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
 }
