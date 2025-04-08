3987b33df58b70f6bf2b2f4cd739d6121e28ee29
orkes-harshil <harshil.raval@orkes.io>
2025-04-08 14:15:21 +0530

    Update SDK POJO () to match server implementation

--- conductor-clients/java/conductor-java-sdk/orkes-client/src/main/java/io/orkes/conductor/client/model/integration/ai/IndexDocInput.java
+++ conductor-clients/java/conductor-java-sdk/orkes-client/src/main/java/io/orkes/conductor/client/model/integration/ai/IndexDocInput.java
@@ -17,10 +17,12 @@ import java.util.Map;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.Builder;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class IndexDocInput {
 
     private String llmProvider;
