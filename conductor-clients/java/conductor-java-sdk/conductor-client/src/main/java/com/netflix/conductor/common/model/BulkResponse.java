b1247422ac15f3123d3ded212f8490a29a0bdcff
orkes-harshil <harshil.raval@orkes.io>
2025-04-09 17:16:06 +0530

    Update SDK POJO () to match server implementation

--- conductor-clients/java/conductor-java-sdk/conductor-client/src/main/java/com/netflix/conductor/common/model/BulkResponse.java
+++ conductor-clients/java/conductor-java-sdk/conductor-client/src/main/java/com/netflix/conductor/common/model/BulkResponse.java
@@ -1,17 +1,19 @@
-/*
- * Copyright 2020 Conductor Authors.
- * <p>
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
- * <p>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
- * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
- * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
- * specific language governing permissions and limitations under the License.
- */
+/* 
+ * Copyright 2020 Conductor Authors. 
+ * <p> 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with 
+ * the License. You may obtain a copy of the License at 
+ * <p> 
+ * http://www.apache.org/licenses/LICENSE-2.0 
+ * <p> 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on 
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the 
+ * specific language governing permissions and limitations under the License. 
+ */ 
 package com.netflix.conductor.common.model;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
@@ -27,9 +29,9 @@ public class BulkResponse<T> {
     /**
      * Key - entityId Value - error message processing this entity
      */
-    private Map<String, String> bulkErrorResults;
+    private Map<String, String> bulkErrorResults = new HashMap<>();
 
-    private List<T> bulkSuccessfulResults;
+    private List<T> bulkSuccessfulResults = new ArrayList<>();
 
     private String message = "Bulk Request has been processed.";
 
