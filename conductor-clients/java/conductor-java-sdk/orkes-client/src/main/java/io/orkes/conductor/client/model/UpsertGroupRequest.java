3a2caf4fea486376cb90d8c5b692c9d2e5143811
orkes-harshil <harshil.raval@orkes.io>
2025-04-09 17:12:52 +0530

    Update SDK POJO () to match server implementation

--- conductor-clients/java/conductor-java-sdk/orkes-client/src/main/java/io/orkes/conductor/client/model/UpsertGroupRequest.java
+++ conductor-clients/java/conductor-java-sdk/orkes-client/src/main/java/io/orkes/conductor/client/model/UpsertGroupRequest.java
@@ -1,4 +1,4 @@
-/*
+/* 
  * Copyright 2022 Conductor Authors.
  * <p>
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
@@ -27,7 +27,7 @@ import lombok.*;
 @AllArgsConstructor
 public class UpsertGroupRequest {
     /**
-     * a default Map&lt;TargetType, Set&lt;Access&gt; to share permissions, allowed target types:
+     * a default Map<TargetType, Set<Access> to share permissions, allowed target types:
      * WORKFLOW_DEF, TASK_DEF
      */
     public enum InnerEnum {
@@ -112,7 +112,7 @@ public class UpsertGroupRequest {
     }
 
     /**
-     * a default Map&lt;TargetType, Set&lt;Access&gt; to share permissions, allowed target types:
+     * a default Map<TargetType, Set<Access> to share permissions, allowed target types:
      * WORKFLOW_DEF, TASK_DEF
      *
      * @return defaultAccess
