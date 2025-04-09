b025ec0bcb84670757a2e4cf8125818c7c311fde
orkes-harshil <harshil.raval@orkes.io>
2025-04-09 17:16:11 +0530

    Update SDK POJO () to match server implementation

--- conductor-clients/java/conductor-java-sdk/conductor-client/src/main/java/com/netflix/conductor/common/run/TaskSummary.java
+++ conductor-clients/java/conductor-java-sdk/conductor-client/src/main/java/com/netflix/conductor/common/run/TaskSummary.java
@@ -1,4 +1,4 @@
-/*
+/* 
  * Copyright 2020 Conductor Authors.
  * <p>
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
@@ -65,5 +65,7 @@ public class TaskSummary {
 
     
     private int workflowPriority;
+
+    private String domain;
 
 }
