c426561e084c3c32194fd9a765be39050ce3ca2f
Robert Karlsson <robert.karlsson1@gmail.com>
2025-04-11 12:05:40 +0900

    run spotless on the query

--- postgres-persistence/src/main/java/com/netflix/conductor/postgres/dao/PostgresQueueDAO.java
+++ postgres-persistence/src/main/java/com/netflix/conductor/postgres/dao/PostgresQueueDAO.java
@@ -449,23 +449,24 @@ public class PostgresQueueDAO extends PostgresBaseDAO implements QueueDAO {
             }
         }
 
-        String POP_QUERY =  "WITH cte AS (" + 
-                            "    SELECT queue_name, message_id " + 
-                            "    FROM queue_message " + 
-                            "    WHERE queue_name = ? " + 
-                            "      AND popped = false " + 
-                            "      AND deliver_on <= (current_timestamp + (1000 || ' microseconds')::interval) " + 
-                            "    ORDER BY priority DESC, deliver_on, created_on " + 
-                            "    LIMIT ? " + 
-                            "    FOR UPDATE SKIP LOCKED " + 
-                            ") " + 
-                            "UPDATE queue_message " + 
-                            "   SET popped = true " + 
-                            "   FROM cte " + 
-                            "   WHERE queue_message.queue_name = cte.queue_name " + 
-                            "     AND queue_message.message_id = cte.message_id " + 
-                            "     AND queue_message.popped = false " + 
-                            "   RETURNING queue_message.message_id, queue_message.priority, queue_message.payload";
+        String POP_QUERY =
+                "WITH cte AS ("
+                        + "    SELECT queue_name, message_id "
+                        + "    FROM queue_message "
+                        + "    WHERE queue_name = ? "
+                        + "      AND popped = false "
+                        + "      AND deliver_on <= (current_timestamp + (1000 || ' microseconds')::interval) "
+                        + "    ORDER BY priority DESC, deliver_on, created_on "
+                        + "    LIMIT ? "
+                        + "    FOR UPDATE SKIP LOCKED "
+                        + ") "
+                        + "UPDATE queue_message "
+                        + "   SET popped = true "
+                        + "   FROM cte "
+                        + "   WHERE queue_message.queue_name = cte.queue_name "
+                        + "     AND queue_message.message_id = cte.message_id "
+                        + "     AND queue_message.popped = false "
+                        + "   RETURNING queue_message.message_id, queue_message.priority, queue_message.payload";
 
         return query(
                 connection,
