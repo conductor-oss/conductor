b9a0333e81bea808c4b448c3166718f9080023ba
narasimhansr <vsnmarch1993@gmail.com>
2025-04-16 21:11:08 +0530

    comm

--- core/src/main/java/com/netflix/conductor/dao/ExecutionDAO.java
+++ core/src/main/java/com/netflix/conductor/dao/ExecutionDAO.java
@@ -55,7 +55,7 @@ public interface ExecutionDAO {
     /**
      * Checks if the number of tasks in progress for the given taskDef will exceed the limit if the
      * task is scheduled to be in progress (given to the worker or for system tasks start() method
-     * called)
+     * called)  
      *
      * @param task The task to be executed. Limit is set in the Task's definition
      * @return true if by executing this task, the limit is breached. false otherwise.
