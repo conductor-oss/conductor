a985c5c391d85b04966b2556efb546ed06835702
orkes-harshil <harshil.raval@orkes.io>
2025-04-24 11:44:24 +0530

    Update SDK POJO () to match server implementation

--- conductor-clients/java/conductor-java-sdk/orkes-client/src/main/java/io/orkes/conductor/client/model/UpsertGroupRequest.java
+++ conductor-clients/java/conductor-java-sdk/orkes-client/src/main/java/io/orkes/conductor/client/model/UpsertGroupRequest.java
@@ -1,4 +1,4 @@
-/*
+/* 
  * Copyright 2022 Conductor Authors.
  * <p>
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
@@ -63,6 +63,7 @@ public class UpsertGroupRequest {
 
     }
 
+    @Deprecated
     private Map<String, List<String>> defaultAccess = null;
 
     private String description = null;
