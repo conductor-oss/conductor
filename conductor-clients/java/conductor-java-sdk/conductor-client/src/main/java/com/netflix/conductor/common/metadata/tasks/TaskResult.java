101cd74516218dfe273a806320681653af806cfc
orkes-harshil <harshil.raval@orkes.io>
2025-04-24 11:46:55 +0530

    Update SDK POJO () to match server implementation

--- conductor-clients/java/conductor-java-sdk/conductor-client/src/main/java/com/netflix/conductor/common/metadata/tasks/TaskResult.java
+++ conductor-clients/java/conductor-java-sdk/conductor-client/src/main/java/com/netflix/conductor/common/metadata/tasks/TaskResult.java
@@ -1,4 +1,4 @@
-/*
+/* 
  * Copyright 2022 Conductor Authors.
  * <p>
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
@@ -69,6 +69,7 @@ public class TaskResult {
 
     private Map<String, Object> outputData = new HashMap<>();
 
+    @Deprecated
     private List<TaskExecLog> logs = new CopyOnWriteArrayList<>();
 
     private String externalOutputPayloadStoragePath;
