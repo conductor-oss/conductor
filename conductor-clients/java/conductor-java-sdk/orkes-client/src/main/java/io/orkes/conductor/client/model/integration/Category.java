3730828950a86f1fdaef45aa39b8f9dffb8c8cc9
orkes-harshil <harshil.raval@orkes.io>
2025-05-07 12:38:29 +0530

    Update SDK POJO (conductor-oss.conductor.io.orkes.conductor.client.model.integration.Category) to match server implementation

--- conductor-clients/java/conductor-java-sdk/orkes-client/src/main/java/io/orkes/conductor/client/model/integration/Category.java
+++ conductor-clients/java/conductor-java-sdk/orkes-client/src/main/java/io/orkes/conductor/client/model/integration/Category.java
@@ -1,4 +1,4 @@
-/*
+/* 
  * Copyright 2024 Conductor Authors.
  * <p>
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
@@ -24,6 +24,8 @@ public enum Category {
 
     MESSAGE_BROKER,
 
-    GIT
+    GIT,
+
+    EMAIL
 
 }
