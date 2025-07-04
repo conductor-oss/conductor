4c53d9be5fa19abee639d87c73d769c7d8e99364
Shailesh Jagannath Padave <shaileshpadave@192.168.1.7>
2025-07-04 14:12:34 +0530

    few more updates

--- common/src/main/java/com/netflix/conductor/common/metadata/tasks/ExecutionMetadata.java
+++ common/src/main/java/com/netflix/conductor/common/metadata/tasks/ExecutionMetadata.java
@@ -12,11 +12,11 @@
  */
 package com.netflix.conductor.common.metadata.tasks;
 
-import com.netflix.conductor.annotations.protogen.ProtoField;
-import com.netflix.conductor.annotations.protogen.ProtoMessage;
-
 import java.util.HashMap;
 import java.util.Map;
+
+import com.netflix.conductor.annotations.protogen.ProtoField;
+import com.netflix.conductor.annotations.protogen.ProtoMessage;
 
 /**
  * Execution metadata for capturing NEW operational metadata not already present in Task/TaskResult models.
@@ -24,168 +24,135 @@ import java.util.Map;
  */
 @ProtoMessage
 public class ExecutionMetadata {
-
+    
     // Direct timing fields
     @ProtoField(id = 1)
     private Long serverSendTime;
-
+    
     @ProtoField(id = 2)
     private Long clientReceiveTime;
-
+    
     @ProtoField(id = 3)
     private Long executionStartTime;
-
+    
     @ProtoField(id = 4)
     private Long executionEndTime;
-
+    
     @ProtoField(id = 5)
     private Long clientSendTime;
-
+    
     @ProtoField(id = 6)
     private Long pollNetworkLatency;
-
+    
     @ProtoField(id = 7)
     private Long updateNetworkLatency;
-
+    
     // Additional context as Map for flexibility
     @ProtoField(id = 8)
     private Map<String, Object> additionalContext = new HashMap<>();
-
-    public ExecutionMetadata() {
-    }
-
+    
+    public ExecutionMetadata() {}
+    
     // ============ TIMING METHODS ============
-
-    /**
-     * Gets server send time
-     */
-    public Long getServerSendTime() {
-        return serverSendTime;
-    }
-
+    
     /**
      * Sets server send time
      */
     public void setServerSendTime(long timestamp) {
         this.serverSendTime = timestamp;
     }
-
-    public void setServerSendTime(Long serverSendTime) {
-        this.serverSendTime = serverSendTime;
-    }
-
-    /**
-     * Gets client receive time
-     */
-    public Long getClientReceiveTime() {
-        return clientReceiveTime;
-    }
-
+    
     /**
      * Sets client receive time
      */
     public void setClientReceiveTime(long timestamp) {
         this.clientReceiveTime = timestamp;
     }
-
-    public void setClientReceiveTime(Long clientReceiveTime) {
-        this.clientReceiveTime = clientReceiveTime;
-    }
-
-    /**
-     * Gets execution start time
-     */
-    public Long getExecutionStartTime() {
-        return executionStartTime;
-    }
-
+    
     /**
      * Sets execution start time
      */
     public void setExecutionStartTime(long timestamp) {
         this.executionStartTime = timestamp;
     }
-
-    public void setExecutionStartTime(Long executionStartTime) {
-        this.executionStartTime = executionStartTime;
-    }
-
-    /**
-     * Gets execution end time
-     */
-    public Long getExecutionEndTime() {
-        return executionEndTime;
-    }
-
+    
     /**
      * Sets execution end time
      */
     public void setExecutionEndTime(long timestamp) {
         this.executionEndTime = timestamp;
     }
-
-    public void setExecutionEndTime(Long executionEndTime) {
-        this.executionEndTime = executionEndTime;
-    }
-
-    /**
-     * Gets client send time
-     */
-    public Long getClientSendTime() {
-        return clientSendTime;
-    }
-
+    
     /**
      * Sets client send time
      */
     public void setClientSendTime(long timestamp) {
         this.clientSendTime = timestamp;
     }
-
-    public void setClientSendTime(Long clientSendTime) {
-        this.clientSendTime = clientSendTime;
-    }
-
-    // ============ ADDITIONAL CONTEXT METHODS ============
-
-    /**
-     * Gets poll network latency
-     */
-    public Long getPollNetworkLatency() {
-        return pollNetworkLatency;
-    }
-
+    
     /**
      * Sets poll network latency
      */
     public void setPollNetworkLatency(long latencyMs) {
         this.pollNetworkLatency = latencyMs;
     }
-
-    public void setPollNetworkLatency(Long pollNetworkLatency) {
-        this.pollNetworkLatency = pollNetworkLatency;
-    }
-
-    // ============ GETTERS AND SETTERS ============
-
-    /**
-     * Gets update network latency
-     */
-    public Long getUpdateNetworkLatency() {
-        return updateNetworkLatency;
-    }
-
+    
     /**
      * Sets update network latency
      */
     public void setUpdateNetworkLatency(long latencyMs) {
         this.updateNetworkLatency = latencyMs;
     }
-
-    public void setUpdateNetworkLatency(Long updateNetworkLatency) {
-        this.updateNetworkLatency = updateNetworkLatency;
+    
+    /**
+     * Gets server send time
+     */
+    public Long getServerSendTime() {
+        return serverSendTime;
     }
-
+    
+    /**
+     * Gets client receive time
+     */
+    public Long getClientReceiveTime() {
+        return clientReceiveTime;
+    }
+    
+    /**
+     * Gets execution start time
+     */
+    public Long getExecutionStartTime() {
+        return executionStartTime;
+    }
+    
+    /**
+     * Gets execution end time
+     */
+    public Long getExecutionEndTime() {
+        return executionEndTime;
+    }
+    
+    /**
+     * Gets client send time
+     */
+    public Long getClientSendTime() {
+        return clientSendTime;
+    }
+    
+    /**
+     * Gets poll network latency
+     */
+    public Long getPollNetworkLatency() {
+        return pollNetworkLatency;
+    }
+    
+    /**
+     * Gets update network latency
+     */
+    public Long getUpdateNetworkLatency() {
+        return updateNetworkLatency;
+    }
+    
     /**
      * Calculates total execution time
      */
@@ -195,36 +162,68 @@ public class ExecutionMetadata {
         }
         return null;
     }
-
+    
+    // ============ ADDITIONAL CONTEXT METHODS ============
+    
     /**
      * Sets additional context data
      */
     public void setAdditionalContext(String key, Object value) {
         additionalContext.put(key, value);
     }
-
+    
     /**
      * Gets additional context data
      */
     public Object getAdditionalContext(String key) {
         return additionalContext.get(key);
     }
-
+    
     /**
      * Gets all additional context data
      */
     public Map<String, Object> getAllAdditionalContext() {
         return new HashMap<>(additionalContext);
     }
-
+    
+    // ============ GETTERS AND SETTERS ============
+    
+    public void setServerSendTime(Long serverSendTime) {
+        this.serverSendTime = serverSendTime;
+    }
+    
+    public void setClientReceiveTime(Long clientReceiveTime) {
+        this.clientReceiveTime = clientReceiveTime;
+    }
+    
+    public void setExecutionStartTime(Long executionStartTime) {
+        this.executionStartTime = executionStartTime;
+    }
+    
+    public void setExecutionEndTime(Long executionEndTime) {
+        this.executionEndTime = executionEndTime;
+    }
+    
+    public void setClientSendTime(Long clientSendTime) {
+        this.clientSendTime = clientSendTime;
+    }
+    
+    public void setPollNetworkLatency(Long pollNetworkLatency) {
+        this.pollNetworkLatency = pollNetworkLatency;
+    }
+    
+    public void setUpdateNetworkLatency(Long updateNetworkLatency) {
+        this.updateNetworkLatency = updateNetworkLatency;
+    }
+    
     public Map<String, Object> getAdditionalContextMap() {
         return additionalContext;
     }
-
+    
     public void setAdditionalContextMap(Map<String, Object> additionalContext) {
         this.additionalContext = additionalContext != null ? additionalContext : new HashMap<>();
     }
-
+    
     @Override
     public String toString() {
         return "ExecutionMetadata{" +
