8d831cbf235c50696a47d0bf26332bd562fad996
rwalkow <radoslaw.walkow@benefitsystems.pl>
2025-07-10 15:49:48 +0200

    Added UI feature to manage event handlers - create/update

---
+++ ui/src/schema/eventHandler.js
@@ -0,0 +1,10 @@
+export const NEW_EVENT_HANDLER_TEMPLATE = {
+  name: "descriptive unique name",
+  event: "event_type:event_location",
+  condition: "boolean condition",
+  actions: ["see examples below"]
+};
+
+export function configureMonaco(monaco) {
+  // No-op
+}
