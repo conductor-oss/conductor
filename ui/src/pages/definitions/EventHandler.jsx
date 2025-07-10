42fe2aa0dbde1ee95c268a87c0b097b898c289d3
rwalkow <radoslaw.walkow@benefitsystems.pl>
2025-07-10 15:49:48 +0200

    Added UI feature to manage event handlers - create/update

--- ui/src/pages/definitions/EventHandler.jsx
+++ ui/src/pages/definitions/EventHandler.jsx
@@ -1,22 +1,26 @@
 import React from "react";
-import { NavLink, DataTable } from "../../components";
+import { NavLink, DataTable, Button } from "../../components";
 import { makeStyles } from "@material-ui/styles";
 import Header from "./Header";
 import sharedStyles from "../styles";
 import { Helmet } from "react-helmet";
 import { useEventHandlers } from "../../data/misc";
+import AddIcon from "@material-ui/icons/Add";
 
 const useStyles = makeStyles(sharedStyles);
 
 const columns = [
   {
     name: "name",
-    renderer: (name) => (
-      <NavLink path={`/eventHandlerDef/${name}`}>{name}</NavLink>
+    renderer: (name, row) => (
+      <NavLink path={`/eventHandlerDef/${row.event}/${name}`}>{name}</NavLink>
     ),
   },
-  { name: "event" },
-  { name: "createTime", type: "date" },
+  {
+    name: "event"
+  },
+  { name: "active", renderer: (val) => val ? "Yes" : "No"  },
+  { name: "condition" },
   {
     name: "actions",
     renderer: (val) => JSON.stringify(val.map((action) => action.action)),
@@ -36,11 +40,17 @@ export default function EventHandlers() {
       </Helmet>
 
       <div className={classes.tabContent}>
+        <div className={classes.buttonRow}>
+          <Button component={NavLink} path="/eventHandlerDef" startIcon={<AddIcon />}>
+            New Event Handler Definition
+          </Button>
+        </div>
+
         {eventHandlers && (
           <DataTable
             title={`${eventHandlers.length} results`}
             localStorageKey="eventHandlersTable"
-            defaultShowColumns={["name", "event", "actions"]}
+            defaultShowColumns={["event", "active", "name", "condition", "actions"]}
             keyField="name"
             data={eventHandlers}
             columns={columns}
