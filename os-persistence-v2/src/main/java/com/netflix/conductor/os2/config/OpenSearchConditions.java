9dac0e54860010b03fa2a1613c2c8f2d42e1d35c
Naomi Most <naomi.most@orkes.io>
2026-01-27 16:51:43 -0800

    Create os-persistence-v2 and os-persistence-v3 modules with shading
    - Created os-persistence-v2 module for OpenSearch 2.x support
      - Package: com.netflix.conductor.os2
      - Condition: @ConditionalOnProperty(indexing.type=opensearch2)
      - Shading: relocates org.opensearch.client to os2.shaded namespace
      - Dependencies: opensearch-java:2.18.0
    
    - Created os-persistence-v3 module for OpenSearch 3.x support
      - Package: com.netflix.conductor.os3
      - Condition: @ConditionalOnProperty(indexing.type=opensearch3)
      - Shading: relocates org.opensearch.client to os3.shaded namespace
      - Dependencies: opensearch-java:3.3.2
    
    - Updated settings.gradle to include both new modules
    - Updated server/build.gradle to include both modules when indexingBackend=opensearch
    
    Both modules use shadow plugin to relocate opensearch-client packages
    to avoid classpath conflicts. Implements unified conductor.indexing.type
    configuration pattern consistent with other backends.
    
    Ref: #678

---
+++ os-persistence-v2/src/main/java/com/netflix/conductor/os2/config/OpenSearchConditions.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2023 Conductor Authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+package com.netflix.conductor.os2.config;
+
+import org.springframework.boot.autoconfigure.condition.AllNestedConditions;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+
+/**
+ * Conditional configuration for enabling OpenSearch 2.x as the indexing backend.
+ *
+ * <p>OpenSearch 2.x is enabled when:
+ *
+ * <ul>
+ *   <li>{@code conductor.indexing.enabled=true} (defaults to true if not specified)
+ *   <li>{@code conductor.indexing.type=opensearch2}
+ * </ul>
+ *
+ * <p><b>Recommended Configuration:</b>
+ *
+ * <pre>{@code
+ * # Enable OpenSearch 2.x indexing
+ * conductor.indexing.enabled=true
+ * conductor.indexing.type=opensearch2
+ *
+ * # OpenSearch connection settings
+ * conductor.opensearch.url=http://localhost:9200
+ * conductor.opensearch.indexPrefix=conductor
+ * conductor.opensearch.indexReplicasCount=0
+ * conductor.opensearch.clusterHealthColor=green
+ * }</pre>
+ */
+public class OpenSearchConditions {
+
+    private OpenSearchConditions() {}
+
+    public static class OpenSearchV2Enabled extends AllNestedConditions {
+
+        OpenSearchV2Enabled() {
+            super(ConfigurationPhase.PARSE_CONFIGURATION);
+        }
+
+        @SuppressWarnings("unused")
+        @ConditionalOnProperty(
+                name = "conductor.indexing.enabled",
+                havingValue = "true",
+                matchIfMissing = true)
+        static class enabledIndexing {}
+
+        @SuppressWarnings("unused")
+        @ConditionalOnProperty(name = "conductor.indexing.type", havingValue = "opensearch2")
+        static class enabledOS2 {}
+    }
+}
