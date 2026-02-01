55edaddf41656f4a0b53b46a4f4beee9535caaa8
Viren Baraiya <virenx@gmail.com>
2026-02-01 12:44:24 -0800

    Update build_ui.sh

--- build_ui.sh
+++ build_ui.sh
@@ -1,7 +1,7 @@
 cd ui
 pwd
 export REACT_APP_ENABLE_ERRORS_INSPECTOR=true
-export REACT_APP_MONACO_EDITOR_USING_CDN=false
+export REACT_APP_MONACO_EDITOR_USING_CDN=true
 yarn install
 yarn build
 echo "Done building UI, copying the UI files to server"
