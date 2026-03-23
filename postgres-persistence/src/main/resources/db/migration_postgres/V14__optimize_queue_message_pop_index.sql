68e48e70fbc38ec8c429b7e1ec01837560183a51
Andrii Stelmashenko <andrey.stelmashenko@gmail.com>
2026-03-23 10:29:23 +0200

    added new index after changing priority order

---
+++ postgres-persistence/src/main/resources/db/migration_postgres/V14__optimize_queue_message_pop_index.sql
@@ -0,0 +1,6 @@
+DROP INDEX IF EXISTS combo_queue_message;
+
+CREATE INDEX combo_queue_message
+    ON queue_message (queue_name, deliver_on ASC, priority DESC, created_on ASC)
+    INCLUDE (message_id)
+    WHERE popped = false;
