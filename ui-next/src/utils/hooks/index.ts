f6bf218cd224607cbbf6ad425b03534f6fa57628
najeebkp <najeebthangalkp@gmail.com>
2026-04-02 14:12:08 +0530

    chore: remove tags dashboard (enterprise-only feature)

--- ui-next/src/utils/hooks/index.ts
+++ ui-next/src/utils/hooks/index.ts
@@ -1,6 +1,4 @@
 export * from "./useCustomPagination";
 export * from "./useEventNameSuggestions";
-export * from "./useBatchedTagsData";
-export * from "./useTagResources";
 export * from "./useGetIntegrations";
 export * from "./useWorkflowNamesAndVersionsQuery";
