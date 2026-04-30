b120c6af0eee9cef0efe64bc9db68b11dd0b2986
Andrew Lai <andrew.lai@orkes.io>
2026-04-29 17:01:41 -0700

    Fix type and prop errors in ExecutionJson after package updates and typescript conversion

--- ui-next/src/pages/execution/ExecutionJson.tsx
+++ ui-next/src/pages/execution/ExecutionJson.tsx
@@ -1,22 +1,12 @@
 import { Box } from "@mui/material";
 import ReactJson from "components/ReactJson";
+import { WorkflowExecution } from "types/Execution";
 
-const EDITOR_THEME_KEY = "editorTheme";
-
-const getReactJsonTheme = () => {
-  let localEditorTheme = localStorage.getItem(EDITOR_THEME_KEY);
-  if (!localEditorTheme) {
-    localEditorTheme = "vs-light";
-  }
-  let theme = "shapeshifter";
-  if (localEditorTheme === "vs-light") {
-    theme = "shapeshifter:inverted";
-  }
-  return theme;
-};
-
-export default function ExecutionJson({ execution }) {
-  const theme = getReactJsonTheme();
+export default function ExecutionJson({
+  execution,
+}: {
+  execution: WorkflowExecution;
+}) {
   return (
     <Box
       sx={{
@@ -30,10 +20,7 @@ export default function ExecutionJson({ execution }) {
     >
       <ReactJson
         src={execution}
-        initialCollapse={true}
         title="Complete Workflow JSON"
-        theme={theme}
-        indentWidth={2}
         workflowName={execution.workflowName}
         editorHeight="100%"
       />
