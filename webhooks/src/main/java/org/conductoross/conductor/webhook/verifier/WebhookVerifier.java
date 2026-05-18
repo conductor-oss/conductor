a03acc534751a98f4fcb059eb14f9e3249b851a4
nthmost-orkes <naomi.most@orkes.io>
2026-05-18 10:05:02 -0700

    refactor(webhooks): drop .tasks. segment from verifier package
    Orkes uses io.orkes.conductor.webhook.verifier (no .tasks. segment).
    Match that layout: org.conductoross.conductor.webhook.verifier.

--- webhooks/src/main/java/org/conductoross/conductor/webhook/verifier/WebhookVerifier.java
+++ webhooks/src/main/java/org/conductoross/conductor/webhook/verifier/WebhookVerifier.java
@@ -10,7 +10,7 @@
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  */
-package org.conductoross.conductor.tasks.webhook.verifier;
+package org.conductoross.conductor.webhook.verifier;
 
 import java.util.Collections;
 import java.util.List;
