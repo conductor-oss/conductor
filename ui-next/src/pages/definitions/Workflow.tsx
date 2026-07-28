d1b93b549405113ae98bba08ad5245a79d0a1409
nicholascole <nicholas.colesd@gmail.com>
2026-07-28 10:54:23 -0700

    fix(ui): exclude agents from workflow definitions

--- ui-next/src/pages/definitions/Workflow.tsx
+++ ui-next/src/pages/definitions/Workflow.tsx
@@ -65,7 +65,7 @@ export default function WorkflowDefinitions() {
   const isPlayground = featureFlags.isEnabled(FEATURES.PLAYGROUND);
   const tagsEnabled = featureFlags.isEnabled(FEATURES.TAG_VISIBILITY);
   const { data, isFetching, refetch }: UseQueryResult<WorkflowDef[]> =
-    useWorkflowDefs();
+    useWorkflowDefs({}, "workflow");
   const [showAddTagDialog, setShowAddTagDialog] = useState(false);
   const [addTagDialogData, setAddTagDialogData] =
     useState<TagDialogProps | null>(null);
